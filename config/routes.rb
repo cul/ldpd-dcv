847372233e1392eaea9413637239c12a1cf79230
Benjamin Armintor <armintor@gmail.com>
2014-05-05 15:04:28 -0400

    thumbnail improvements

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Dcv::Application.routes.draw do
       post "track"
     end
   end
+
+  resources :thumbs, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
