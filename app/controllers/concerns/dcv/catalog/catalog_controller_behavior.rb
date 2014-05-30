4ff5a898c306afc677a6cbe79a2caceb4cd3795f
Eric O <elo2112@columbia.edu>
2014-05-30 12:51:45 -0400

    Updated Collections page to include 'View Content' and 'Jump to Project Site' links; Fix for exclusion facet code

--- app/controllers/concerns/dcv/catalog/catalog_controller_behavior.rb
+++ app/controllers/concerns/dcv/catalog/catalog_controller_behavior.rb
@@ -7,7 +7,7 @@ module Dcv::Catalog::CatalogControllerBehavior
 
   def exclude_groups_and_files_by_default(solr_parameters, user_parameters)
     if params[:search].blank?
-        params[:f] = {}
+        params[:f] ||= {}
         params[:f]['active_fedora_model_ssi'] = ['exclude_groups', 'exclude_files']
         params[:search] = 'true'
     end
