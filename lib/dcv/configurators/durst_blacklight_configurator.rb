a2ed4c22cc2864587dc7d0c92ac87b90f5aaf0fc
Eric O <elo2112@columbia.edu>
2014-09-18 15:42:00 -0400

    Updated durst config project filter

--- lib/dcv/configurators/durst_blacklight_configurator.rb
+++ lib/dcv/configurators/durst_blacklight_configurator.rb
@@ -9,7 +9,7 @@ class Dcv::Configurators::DurstBlacklightConfigurator
     config.show.route = { controller: 'durst' }
 
     config.default_solr_params = {
-      :fq => 'lib_project_short_ssim:"G.E.E. Lindquist Native American Photographs"',
+      :fq => 'lib_project_short_ssim:"Lindquist Photographs"',
       :qt => 'search',
       :rows => 20
     }
