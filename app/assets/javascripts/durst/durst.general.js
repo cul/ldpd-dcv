11c22e0b05a6e98fe971603068af29f49c7da191
erik r <er2576@columbia.edu>
2014-10-01 16:29:44 -0400

    durst PROTOTYPE post-caf ui mods

--- app/assets/javascripts/durst/durst.general.js
+++ app/assets/javascripts/durst/durst.general.js
@@ -1,13 +1,30 @@
 $(function() {
 
  if ($('#carousel-example-generic').length) {  $('#durst-alt-home').removeClass('hide'); }
- $('body').on('click', '#durst-alt-home', function() {
+ $('body').on('click', '#durst-alt-home, #mapholder-link', function() {
    if ($('#content').hasClass('col-md-9')) {
      $('#content').removeClass('col-md-9').addClass('col-md-6');
+     $('#mapholder-link').removeClass('hide');
+     $('#durst_osm').addClass('hide');
    } else {
      $('#content').removeClass('col-md-6').addClass('col-md-9');
+     $('#mapholder-link').addClass('hide');
+     $('#durst_osm').removeClass('hide').attr('src', $('#durst_osm').attr('src'));
    }
-   $('#dhss').toggleClass('hide').find('.inner').height($('#content').find('.inner').height());
+   $('#dhss').toggleClass('hide');
    return false;
  });
 });
+$(window).load(function() {
+   $('#dhss').find('.inner img').height($('#content .inner img').height());
+});
+
+function resizedw(){
+    // Haven't resized in 100ms!
+   $('#dhss').find('.inner img').height($('#content .inner img').height());
+}
+var dorsz;
+window.onresize = function(){
+  clearTimeout(dorsz);
+  dorsz = setTimeout(resizedw, 100);
+};
