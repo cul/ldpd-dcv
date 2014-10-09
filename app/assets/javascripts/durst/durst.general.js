160bf3ca5c59df6716f01f458870ba1cdb146e4a
erik r <er2576@columbia.edu>
2014-10-09 12:35:59 -0400

    durst PROTO map and carousel control tweaks.

--- app/assets/javascripts/durst/durst.general.js
+++ app/assets/javascripts/durst/durst.general.js
@@ -97,7 +97,7 @@ function homeMap() {
     //var mapurl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     //var mapurl='http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg';
     var mapurl='http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}';
-    map = L.map('durst_osm').setView([40.7830, -73.9052], 12);
+    map = L.map('durst_osm').setView([40.7730, -73.8402], 11);
     L.tileLayer(mapurl, {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>. Tiles &copy; Esri&mdash;National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC.',
     maxZoom: 18
