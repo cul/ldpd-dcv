3eaa1bb3cace9fd19d3cc6321229a58f64a476a7
Eric O <elo2112@columbia.edu>
2014-11-05 13:28:15 -0500

    Durst config update for sample durst records

--- lib/dcv/configurators/durst_blacklight_configurator.rb
+++ lib/dcv/configurators/durst_blacklight_configurator.rb
@@ -9,7 +9,7 @@ class Dcv::Configurators::DurstBlacklightConfigurator
     config.show.route = { controller: 'durst' }
 
     config.default_solr_params = {
-      :fq => 'lib_project_short_ssim:"Lindquist Photographs"',
+      :fq => 'lib_project_short_ssim:"Durst"',
       :qt => 'search',
       :rows => 20
     }
