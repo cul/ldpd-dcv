63bccf4229d7d7a052c209968a8f5d6cf37dddd3
Eric O <elo2112@columbia.edu>
2014-11-06 16:23:55 -0500

    Get all map points for Durst home page

--- app/controllers/durst_controller.rb
+++ app/controllers/durst_controller.rb
@@ -5,8 +5,25 @@ class DurstController < SubsitesController
   end
 
   def index
-    super
-    render 'home' unless has_search_parameters?
+    if has_search_parameters?
+      super
+    else
+      # Special logic for home page.  This is temporary.
+      # Once we have a lot of records in solr, we'll want to do the home page
+      # map query via ajax request to a different controller action.
+      (@response, @document_list) = get_search_results(params, {:rows => 100000}) # Calling get_search_results manually so that we always plot all points for the home page map
+      respond_to do |format|
+        format.html { }
+        format.rss  { render :layout => false }
+        format.atom { render :layout => false }
+        format.json do
+          render json: render_search_results_as_json
+        end
+        additional_response_formats(format)
+        document_export_formats(format)
+      end
+      render 'home'
+    end
   end
 
 end
