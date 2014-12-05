f5e055141a85df6054861ac44898c1da0c26028d
erik r <er2576@columbia.edu>
2014-12-05 14:03:14 -0500

    ifp partner controler

--- app/controllers/ifp_controller.rb
+++ app/controllers/ifp_controller.rb
@@ -11,5 +11,14 @@ class IfpController < SubsitesController
       render 'home'
     end
   end
+ 
+  def partner
+    if params[:key].index('..') == nil && File.exists?(Rails.root.join("app", "views", 'ifp/partner', "#{params[:key]}.html.erb")) 
+      partial_file_path = 'ifp/partner/'+params[:key]
+      render partial_file_path
+    else
+      render 'ifp/partner/not_found'
+    end
+  end
 
 end
