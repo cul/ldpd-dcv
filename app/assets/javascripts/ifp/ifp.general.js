32114d923d4507da5429f52f710a5374b0000cc7
erik r <er2576@columbia.edu>
2014-12-19 13:12:35 -0500

    IFP partner WIP

--- app/assets/javascripts/ifp/ifp.general.js
+++ app/assets/javascripts/ifp/ifp.general.js
@@ -1,1 +1,9 @@
-
+$(function() {
+/* 
+ // will do on backend instead
+  if ( $('body').hasClass('blacklight-ifp-partner') ) {
+    var fullt = $('.partner-text').text();
+    $('.partner-text-excerpt').html(fullt.substring(0, 200)+'&hellip;')
+  }
+*/
+});
