4ca3586223f4c8923c8ac45f30c7513c6551bbd0
Eric O <elo2112@columbia.edu>
2015-01-07 17:02:54 -0500

    Added caption under IFP partner videos

--- app/helpers/dcv/media_element_helper.rb
+++ app/helpers/dcv/media_element_helper.rb
@@ -6,8 +6,8 @@ module Dcv::MediaElementHelper
           <source type="video/rtmp" src="rtmp://firehose.cul.columbia.edu:1935/' + wowza_project + '/mp4:' + video_path + '" />
 
           <!-- Flash fallback for non-HTML5 browsers without JavaScript -->
-          <object width="320" height="180" type="application/x-shockwave-flash" data="' + asset_path('mediaelement/flashmediaelement.swf') + '">
-              <param name="movie" value="' + asset_path('mediaelement/flashmediaelement.swf') + '" />
+          <object width="320" height="180" type="application/x-shockwave-flash" data="/mediaelement/flashmediaelement.swf">
+              <param name="movie" value="/mediaelement/flashmediaelement.swf" />
               <param name="flashvars" value="controls=true&amp;file=rtmp://firehose.cul.columbia.edu:1935/' + wowza_project + '/mp4:' + video_path + '" />
               <param name="allowFullScreen" value="true" />
           </object>
