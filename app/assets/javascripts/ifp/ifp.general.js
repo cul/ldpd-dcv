d7a4810fdf0a11a154236ec4fd049c107b232a7e
erik r <er2576@columbia.edu>
2015-02-17 12:47:27 -0500

    IFP list-mode sizing change / override js and css

--- app/assets/javascripts/ifp/ifp.general.js
+++ app/assets/javascripts/ifp/ifp.general.js
@@ -37,3 +37,28 @@ DCV.PreviewModal.show = function(displayUrl, title){
   return false;
 };
 
+
+DCV.SearchResults.setSearchMode = function(searchMode) {
+  $('.result-type-button').removeClass('btn-success').addClass('btn-default');
+
+  if (searchMode == 'grid') {
+      $('#content .document').removeClass('col-sm-12').removeClass('list-view');
+      $('#content .document').find('h3').addClass('ellipsis');
+      $('#content .document .tombstone').removeClass('row');
+      $('#content .document .thumbnail').removeClass('col-sm-1');
+      $('#content .index-show-list-fields').addClass('hidden');
+      $('#content .index-show-tombstone-fields').removeClass('hidden');
+      $('#grid-mode').addClass('btn-success');
+      createCookie(DCV.SearchResults.CookieNames.searchMode, 'grid', 1);
+  } else if (searchMode == 'list') {
+      $('#content .document').addClass('col-sm-12').addClass('list-view');
+      $('#content .document').find('h3').removeClass('ellipsis');
+      $('#content .document .thumbnail').addClass('col-sm-1');
+      $('#content .index-show-tombstone-fields').addClass('hidden');
+      $('#content .index-show-list-fields').removeClass('hidden');
+      $('#list-mode').addClass('btn-success');
+      createCookie(DCV.SearchResults.CookieNames.searchMode, 'list', 1);
+  } else {
+    //alert('Invalid search mode: ' + searchMode);
+  }
+}
