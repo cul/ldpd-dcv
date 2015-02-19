1e643305d23f48cb3da79dadb8c0b82599cadab2
Eric O <eric@ejdigitalmedia.com>
2015-02-19 00:30:21 -0500

    Show Generic Resource redesign; Fixes for Generic Resource zooming image viewer

--- lib/dcv/configurators/ifp_blacklight_configurator.rb
+++ lib/dcv/configurators/ifp_blacklight_configurator.rb
@@ -72,7 +72,8 @@ class Dcv::Configurators::IfpBlacklightConfigurator
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field ActiveFedora::SolrService.solr_name('title_display', :displayable, type: :string), :label => 'Title', :separator => '; '
+    config.add_show_field ActiveFedora::SolrService.solr_name('contributor', :symbol, type: :string), :label => 'Office'
+    config.add_show_field ActiveFedora::SolrService.solr_name('original_name', :symbol), :label => 'Folder Path', :helper_method => :dirname_prefixed_with_slash
     config.add_show_field ActiveFedora::SolrService.solr_name('identifier', :symbol), :label => 'Identifier', :separator => '; '
 
     # "fielded" search configuration. Used by pulldown among other places.
