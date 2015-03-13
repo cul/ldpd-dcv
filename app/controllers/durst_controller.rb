1dd8b844439dbea8ba9ef30cad291aded4578e5c
Eric O <elo2112@columbia.edu>
2015-03-13 14:44:30 -0400

    Durst map performance improvements

--- app/controllers/durst_controller.rb
+++ app/controllers/durst_controller.rb
@@ -1,5 +1,7 @@
 class DurstController < SubsitesController
 
+  helper_method :extract_map_data_from_document_list
+
   configure_blacklight do |config|
     Dcv::Configurators::DurstBlacklightConfigurator.configure(config)
   end
@@ -12,19 +14,51 @@ class DurstController < SubsitesController
   end
 
   def map_search
-    # Special logic for map search.  Need to pull in ALL records.
-
-    @response = Rails.cache.fetch('map_data_response')
-    @document_list = Rails.cache.fetch('map_data_document_list')
-
-    if @response.nil?
-      #Rails.logger.info('Retrieving and caching Durst map data.')
+    
+    @map_data_json = Rails.cache.fetch('map_data_json')
+    if @map_data_json.nil?
       (@response, @document_list) = get_search_results(params, {:rows => 100000, :fl => 'id, geo, lib_format_ssm, title_display_ssm'}) # Calling get_search_results manually so that we always plot all points for the home page map
-      cache_expiration_time = 10.minutes
-      Rails.cache.write('map_data_response', @response, expires_in: cache_expiration_time)
-      Rails.cache.write('map_data_document_list', @document_list, expires_in: cache_expiration_time)
+      cache_expiration_time = 12.hours
+      map_data = extract_map_data_from_document_list(@document_list)
+      @map_data_json = map_data.to_json
+      Rails.cache.write('map_data_json', @map_data_json, expires_in: cache_expiration_time)
     end
 
+  end
+
+  private
+
+  def extract_map_data_from_document_list(document_list)
+
+    # We want this data to be as compact as possible because we're sending a lot to the client
+
+    max_title_length = 50
+
+    map_data = []
+    document_list.each do |document|
+      if document['geo'].present?
+        document['geo'].each do |coordinates|
+
+          lat_and_long = coordinates.split(',')
+
+          is_book = document['lib_format_ssm'].present? && document['lib_format_ssm'].include?('books')
+
+          title = document['title_display_ssm'][0].gsub(/\s+/, ' ') # Compress multiple spaces and new lines into one
+          title = title[0,max_title_length].strip + '...' if title.length > max_title_length
+
+          row = {
+            id: document.id,
+            c: lat_and_long[0].strip + ',' + lat_and_long[1].strip,
+            t: title,
+            b: is_book ? 'y' : 'n',
+          }
+
+          map_data << row
+        end
+      end
+    end
+
+    return map_data
   end
 
 end
--- app/helpers/dcv/map_data_helper.rb
+++ app/helpers/dcv/map_data_helper.rb
@@ -1,34 +1,34 @@
 module Dcv::MapDataHelper
 
-  def get_map_data_for_document_list(document_list)
-
-    start_time = Time.now
-
-    coordinate_output = []
-    document_list.each do |document|
-      if document['geo'].present?
-        document['geo'].each do |coordinates|
-          lat_and_long = coordinates.split(',')
-
-          if document['lib_format_ssm'].present? && document['lib_format_ssm'].include?('books')
-            image_url_for_document = image_url('book-placeholder.png')
-          else
-            image_url_for_document = get_asset_url(id: document.id, size: 256, type: 'square', format: 'jpg')
-          end
-
-          row = {
-            lat: lat_and_long[0].strip, long: lat_and_long[1].strip, title: document['title_display_ssm'][0],
-            thumbnail_url: image_url_for_document,
-            item_link: '/durst/' + document.id
-          }
-          coordinate_output << row
-        end
-      end
-    end
-
-    puts 'Processed and formatted map data in ' + (Time.now - start_time).to_s + ' seconds'
-
-    return coordinate_output
-  end
+  #def get_map_data_for_document_list(document_list)
+  #
+  #  start_time = Time.now
+  #
+  #  coordinate_output = []
+  #  document_list.each do |document|
+  #    if document['geo'].present?
+  #      document['geo'].each do |coordinates|
+  #        lat_and_long = coordinates.split(',')
+  #
+  #        if document['lib_format_ssm'].present? && document['lib_format_ssm'].include?('books')
+  #          image_url_for_document = image_url('book-placeholder.png')
+  #        else
+  #          image_url_for_document = get_asset_url(id: document.id, size: 256, type: 'square', format: 'jpg')
+  #        end
+  #
+  #        row = {
+  #          lat: lat_and_long[0].strip, long: lat_and_long[1].strip, title: document['title_display_ssm'][0],
+  #          thumbnail_url: image_url_for_document,
+  #          item_link: '/durst/' + document.id
+  #        }
+  #        coordinate_output << row
+  #      end
+  #    end
+  #  end
+  #
+  #  puts 'Processed and formatted map data in ' + (Time.now - start_time).to_s + ' seconds'
+  #
+  #  return coordinate_output
+  #end
 
 end
