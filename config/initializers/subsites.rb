1f557c6fa4652f7982fd86cf0b7b4951aadde0c9
Eric O <eric@ejdigitalmedia.com>
2015-04-02 15:54:44 -0400

    Don't include Durst subsite in public dcv for now

--- config/initializers/subsites.rb
+++ config/initializers/subsites.rb
@@ -2,6 +2,12 @@ SUBSITES = YAML.load_file("#{Rails.root.to_s}/config/subsites.yml")[Rails.env]
 
 # Certain sites shouldn't be present on prod
 if Rails.env == 'dcv_prod'
+
+  # Don't show IFP in public for now
   SUBSITES['public'].delete('ifp')
   SUBSITES['restricted'].delete('ifp')
+
+  # Don't show Durst in public for now
+  SUBSITES['public'].delete('durst')
+
 end
