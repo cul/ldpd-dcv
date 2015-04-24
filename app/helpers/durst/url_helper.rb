51f1c0fd605c419562dc15e06c5f57cf2b040184
Eric O <eric@ejdigitalmedia.com>
2015-04-23 23:35:58 -0400

    Durst subjects page

--- app/helpers/durst/url_helper.rb
+++ app/helpers/durst/url_helper.rb
@@ -15,5 +15,9 @@ module Durst::UrlHelper
 	def durst_facet_search_url(facet_field_name, value)
 		return url_for({controller: 'durst', action: 'index', search_field: 'all_text_teim', q: '', 'f' => {facet_field_name => [value]}})
 	end
+	
+	def durst_subject_search_url(subject_term_value)
+		return url_for({controller: 'durst', action: 'index', search_field: 'all_text_teim', q: '', 'f' => {'durst_subjects_ssim' => [subject_term_value]}})
+	end
 
 end
