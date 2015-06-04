3b0183dcfb401cdb043b2d0403cad4ae206b6ae8
Benjamin Armintor <armintor@gmail.com>
2015-06-04 16:02:21 -0400

    use the subsite config to config subsites

--- config/initializers/subsites.rb
+++ config/initializers/subsites.rb
@@ -1,15 +1,1 @@
 SUBSITES = YAML.load_file("#{Rails.root.to_s}/config/subsites.yml")[Rails.env]
-
-# Certain sites shouldn't be present on prod
-if Rails.env == 'dcv_prod'
-
-  # Disable the following restricted subsites in prod
-  #SUBSITES['restricted'].delete('subsite_name')
-  SUBSITES['restricted'].delete('ifp')
-  SUBSITES['restricted'].delete('universityseminars')
-
-  # Disable the following public subsites in prod
-  #SUBSITES['public'].delete('subsite_name')
-  SUBSITES['public'].delete('jay')
-
-end
