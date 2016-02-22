828bcd5a76bd6e8a0e32e7c3f889c047ecd8edf8
Eric O <eric@ejdigitalmedia.com>
2016-02-22 18:50:56 -0500

    Version 0.8.12; Update cul_hydra

--- app/helpers/durst/field_formatter_helper.rb
+++ app/helpers/durst/field_formatter_helper.rb
@@ -26,25 +26,40 @@ module Durst::FieldFormatterHelper
 
 	def render_url_and_catalog_links(document, as_dl=true)
 
-    urls = document['lib_non_item_in_context_url_ssm'] || []
-    clio_ids = document['clio_ssim'] || []
-
-    if as_dl
-      return (
-        '<dl class="dl-horizontal">' +
-        (urls.length == 0 ? '' : '<dt>Online:</dt><dd> ' + urls.map{|url| link_to('click here for full-text <span class="glyphicon glyphicon-new-window"></span>'.html_safe, url, target: '_blank') }.join('</dd><dt></dt><dd>') + '</dd>') +
-        (clio_ids.length == 0 ? '' : '<dt>Catalog Record:</dt><dd> ' + clio_ids.map{|clio_id| link_to('check availability <span class="glyphicon glyphicon-new-window"></span>'.html_safe, 'http://clio.columbia.edu/catalog/' + clio_id, target: '_blank') }.join('</dd><dt></dt><dd>') + '</dd>') +
-        '</dl>'
-      ).html_safe
-    else
-      return (
-        (urls.length == 0 ? '' : '<strong>Online:</strong> ' + urls.map{|url| link_to('click here for full-text <span class="glyphicon glyphicon-new-window"></span>'.html_safe, url, target: '_blank') }.join('; ')) +
-        (urls.length > 0 && clio_ids.length > 0 ? '<br />' : '') +
-        (clio_ids.length == 0 ? '' : '<strong>Catalog Record:</strong> ' + clio_ids.map{|clio_id| link_to('check availability <span class="glyphicon glyphicon-new-window"></span>'.html_safe, 'http://clio.columbia.edu/catalog/' + clio_id, target: '_blank')}.join('; '))
-      ).html_safe
-    end
+		urls = document['lib_non_item_in_context_url_ssm'] || []
+		clio_ids = document['clio_ssim'] || []
+	
+		if as_dl
+		  return (
+			'<dl class="dl-horizontal">' +
+			(urls.length == 0 ? '' : '<dt>Online:</dt><dd> ' + urls.map{|url| link_to('click here for full-text <span class="glyphicon glyphicon-new-window"></span>'.html_safe, url, target: '_blank') }.join('</dd><dt></dt><dd>') + '</dd>') +
+			(clio_ids.length == 0 ? '' : '<dt>Catalog Record:</dt><dd> ' + clio_ids.map{|clio_id| link_to('check availability <span class="glyphicon glyphicon-new-window"></span>'.html_safe, 'http://clio.columbia.edu/catalog/' + clio_id, target: '_blank') }.join('</dd><dt></dt><dd>') + '</dd>') +
+			'</dl>'
+		  ).html_safe
+		else
+		  return (
+			(urls.length == 0 ? '' : '<strong>Online:</strong> ' + urls.map{|url| link_to('click here for full-text <span class="glyphicon glyphicon-new-window"></span>'.html_safe, url, target: '_blank') }.join('; ')) +
+			(urls.length > 0 && clio_ids.length > 0 ? '<br />' : '') +
+			(clio_ids.length == 0 ? '' : '<strong>Catalog Record:</strong> ' + clio_ids.map{|clio_id| link_to('check availability <span class="glyphicon glyphicon-new-window"></span>'.html_safe, 'http://clio.columbia.edu/catalog/' + clio_id, target: '_blank')}.join('; '))
+		  ).html_safe
+		end
 
 	end
+	
+	def render_durst_location_information(document)
+		
+		sublocation = document['lib_sublocation_ssm'].present? ? document['lib_sublocation_ssm'][0] : ''
+		collection = document['lib_collection_ssm'].present? ? document['lib_collection_ssm'][0] : ''
+		shelf_location = document['location_shelf_locator_ssm'].present? ? document['location_shelf_locator_ssm'][0] : ''
+		
+		# Example output: "Avery Classics Collection, Seymour B. Durst Old York Library Collection, Box no. 35, Item no. 353."
+		return	(
+			'<dl class="dl-horizontal">' +
+				'<dt>Location:</dt>' +
+				'<dd>' + (link_to(sublocation, 'http://library.columbia.edu/locations/avery/classics.html') + ', ' + collection + ', ' + shelf_location + '.') + '</dd>' +
+			'</dl>'
+		).html_safe
+    end
 	
 	def combined_field_published_string(document)
 
