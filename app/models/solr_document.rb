94ede72017cddf6affef6f99d8f865caf3c26e10
Eric O <eric@ejdigitalmedia.com>
2016-04-13 14:00:47 -0400

    Version 0.9.08; Aggregate and display child Object in Context links on parent show page

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -16,6 +16,35 @@ class SolrDocument
   # single valued. See Blacklight::Solr::Document::ExtendableClassMethods#field_semantics
   # and Blacklight::Solr::Document#to_semantic_values
   # Recommendation: Use field names from Dublin Core
-  use_extension( Blacklight::Solr::Document::DublinCore)    
+  use_extension( Blacklight::Solr::Document::DublinCore)
+  
+  def get_item_in_context_urls()
+    @item_in_context_urls ||= aggregate_item_in_context_urls()
+  end
+  
+  private
+  
+  # Aggregate item in context urls from this solr document and all direct member solr documents
+  def aggregate_item_in_context_urls()
+    urls = []
+    urls += self['lib_item_in_context_url_ssm'] if self['lib_item_in_context_url_ssm'].present?
+    
+    search_response = Blacklight.solr.get 'select', :params => {
+      :q  => '*:*',
+      :fl => 'lib_item_in_context_url_ssm',
+      :qt => 'search',
+      :fq => [
+        'cul_member_of_ssim:"info:fedora/' + self['id'] + '"',
+      ],
+      :rows => 999,
+      :facet => false
+    }
+    
+    search_response['response']['docs'].each do |doc|
+      urls += doc['lib_item_in_context_url_ssm'] if doc['lib_item_in_context_url_ssm'].present?
+    end
+    
+    urls
+  end
 
 end
