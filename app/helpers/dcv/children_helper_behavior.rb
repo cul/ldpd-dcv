9b66856b916f726035f29f7a59d984bec519ca2e
erik r <er2576@columbia.edu>
2016-05-06 11:21:43 -0400

    IFP-59. changed title attr on filesystem view info-sign link to "More information" as per request

--- app/helpers/dcv/children_helper_behavior.rb
+++ app/helpers/dcv/children_helper_behavior.rb
@@ -127,7 +127,7 @@ module Dcv::ChildrenHelperBehavior
       if node['pid'] 
         content_tag(:tr,nil) do
           c = ('<td data-title="Name">'+download_link(node, label, ['fs-file',html_class_for_filename(node['label_ssi'])])+' '+ 
-            link_to('<span class="glyphicon glyphicon-info-sign"></span>'.html_safe, url_to_item(node['pid'],{return_to_filesystem:request.original_url}), title: 'Item permanent link')+
+            link_to('<span class="glyphicon glyphicon-info-sign"></span>'.html_safe, url_to_item(node['pid'],{return_to_filesystem:request.original_url}), title: 'More information')+
             '</td>').html_safe
           c += ('<td data-title="Size" data-sort-value="'+node['extent'].join(",").to_s+'">'+filesize+'</td>').html_safe
           #c += content_tag(:a, 'Preview', href: '#', 'data-url'=>url_to_preview(node['pid']), class: 'preview') do 
