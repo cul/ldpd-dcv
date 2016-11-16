2b2bd92bc3d2a28f54de15b7eb72d8a9ec86e3bf
Benjamin Armintor <armintor@gmail.com>
2016-11-16 08:38:44 -0500

    WIP DCV-441 initializer resilient to missing yml for testing

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,9 +1,11 @@
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
-RESQUE_CONFIG = ActiveSupport::HashWithIndifferentAccess.new(YAML.load_file(rails_root + '/config/resque.yml')[rails_env])
+resque_config_path = File.join(Rails.root, 'config', 'resque.yml')
+resque_config = File.exist?(resque_config_path) ?
+  YAML.load_file(resque_config_path)[rails_env] : {}
+RESQUE_CONFIG = ActiveSupport::HashWithIndifferentAccess.new(resque_config)
 
 Resque.redis = RESQUE_CONFIG['url']
-Resque.redis.namespace = 'resque:' + RESQUE_CONFIG['namespace']
+Resque.redis.namespace = 'resque:' + RESQUE_CONFIG['namespace'] if RESQUE_CONFIG['namespace']
 
 Resque.logger = MonoLogger.new(File.open("#{Rails.root}/log/resque.log", "w+"))
 Resque.logger.formatter = Resque::VerboseFormatter.new
