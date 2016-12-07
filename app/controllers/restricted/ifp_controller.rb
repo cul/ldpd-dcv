cc915c6337947be9743f7daafa4e98133a2fd730
Eric O <eric@ejdigitalmedia.com>
2016-12-07 18:10:53 -0500

    Update restricted ifp and universityseminars controllers to include pub-target-based item selection

--- app/controllers/restricted/ifp_controller.rb
+++ app/controllers/restricted/ifp_controller.rb
@@ -3,6 +3,8 @@ class Restricted::IfpController < SubsitesController
   configure_blacklight do |config|
     Dcv::Configurators::Restricted::IfpBlacklightConfigurator.configure(config)
     Dcv::Configurators::FullTextConfigurator.configure(config)
+    # Include only this target's content in search results
+    config.default_solr_params[:fq] << "publisher_ssim:\"#{subsite_config['uri']}\""
   end
 
   def index
--- app/controllers/restricted/universityseminars_controller.rb
+++ app/controllers/restricted/universityseminars_controller.rb
@@ -3,6 +3,8 @@ class Restricted::UniversityseminarsController < SubsitesController
   configure_blacklight do |config|
     Dcv::Configurators::Restricted::UniversityseminarsBlacklightConfigurator.configure(config)
     Dcv::Configurators::FullTextConfigurator.configure(config)
+    # Include only this target's content in search results
+    config.default_solr_params[:fq] << "publisher_ssim:\"#{subsite_config['uri']}\""
   end
 
   def index
