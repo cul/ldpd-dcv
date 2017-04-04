fcb50ff548a042a88989260f74ceb9a7137559fc
Benjamin Armintor <armintor@gmail.com>
2017-04-04 17:10:50 -0400

    compact nils out of configured sites value array

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -6,6 +6,7 @@ class CatalogController < SubsitesController
     Dcv::Configurators::DcvBlacklightConfigurator.configure(config)
     # Include this target's content in search results, and any additional publish targets specified in subsites.yml
     publishers = [subsite_config['uri']] + (subsite_config['additional_publish_targets'] || [])
+    publishers.compact!
     config.default_solr_params[:fq] << 'publisher_ssim:("' + publishers.join('" OR "') + '")'
     
     # Do not include the catalog publish target or any additional publish targets defined in search results
