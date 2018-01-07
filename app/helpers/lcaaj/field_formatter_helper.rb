acbb9c3700d911647890a996ff119d837b862a04
Eric O <eric@ejdigitalmedia.com>
2018-01-07 01:10:18 -0500

    LCAAJ facet helper update

--- app/helpers/lcaaj/field_formatter_helper.rb
+++ app/helpers/lcaaj/field_formatter_helper.rb
@@ -2,8 +2,6 @@ module Lcaaj::FieldFormatterHelper
   def lcaaj_format_form_document_type(value)
     if value == 'printouts'
 		  'Computer Printout'
-    elsif value == 'manuscripts'
-  		nil
     else
       value.split(' ').map(&:capitalize).join(' ').singularize
     end
