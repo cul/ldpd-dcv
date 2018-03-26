9954fe71c5edf7f0058c0901f9945d1b60b9ce03
Eric O <eric@ejdigitalmedia.com>
2018-03-26 14:04:07 -0400

    DCV-535: Show dynamically-retrieved, rounded asset count on DLC home page

--- spec/helpers/catalog_helper_spec.rb
+++ spec/helpers/catalog_helper_spec.rb
@@ -12,67 +12,92 @@ require 'rails_helper'
 # end
 
 describe CatalogHelper, :type => :helper do
-  before do
-    allow(helper).to receive(:document_show_link_field).and_return(document_show_link_field)
-  end
-  let(:document) do
-    {
-      'title_short' => '0123456789abc',
-      'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
-      'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz']
-    }
-  end
-  describe '#short_title' do
-    subject { helper.short_title(document) }
-    context "a short title" do
+  context do
+    before do
+      allow(helper).to receive(:document_show_link_field).and_return(document_show_link_field)
+    end
+    let(:document) do
+      {
+        'title_short' => '0123456789abc',
+        'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
+        'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz']
+      }
+    end
+    describe '#short_title' do
+      subject { helper.short_title(document) }
+      context "a short title" do
+        let(:document_show_link_field) { 'title_short' }
+        it { is_expected.to eql('0123456789abc') }
+      end
+      context "a long title" do
+        let(:document_show_link_field) { 'title_long' }
+        it { is_expected.to eql('0123456789abcdefghijklmnopq...') }
+      end
+      context "a long title in an array" do
+        let(:document_show_link_field) { 'title_long_array' }
+        it { is_expected.to eql('0123456789abcdefghijklmnopq...') }
+      end
+      context "no title" do
+        let(:document_show_link_field) { 'title_absent' }
+        it { is_expected.to be_nil }
+      end
+    end
+    describe '#url_for_document' do
+      let(:slug) { 'sluggo' }
       let(:document_show_link_field) { 'title_short' }
-      it { is_expected.to eql('0123456789abc') }
-    end
-    context "a long title" do
-      let(:document_show_link_field) { 'title_long' }
-      it { is_expected.to eql('0123456789abcdefghijklmnopq...') }
-    end
-    context "a long title in an array" do
-      let(:document_show_link_field) { 'title_long_array' }
-      it { is_expected.to eql('0123456789abcdefghijklmnopq...') }
-    end
-    context "no title" do
-      let(:document_show_link_field) { 'title_absent' }
-      it { is_expected.to be_nil }
+      subject { helper.url_for_document(SolrDocument.new(document)) }
+      context 'with a site result' do
+        let(:document) do
+          {
+            'title_short' => '0123456789abc',
+            'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
+            'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz'],
+            'dc_type_ssm' => ['Publish Target'],
+            'slug_ssim' => [slug]
+          }
+        end
+        it { is_expected.to eql('/sites/' + slug) }
+      end
+      context 'with a non-site result' do
+        let(:document) do
+          {
+            'title_short' => '0123456789abc',
+            'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
+            'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz'],
+            'dc_type_ssm' => ['Unpublish Target'],
+            'slug_ssim' => [slug]
+          }
+        end
+        # until we configure routes in this helper config
+        it { is_expected.to be_a SolrDocument }
+      end
+      context 'with nil' do
+        subject { helper.url_for_document(nil) }
+        it { is_expected.to be_nil }
+      end
     end
   end
-  describe '#url_for_document' do
-    let(:slug) { 'sluggo' }
-    let(:document_show_link_field) { 'title_short' }
-    subject { helper.url_for_document(SolrDocument.new(document)) }
-    context 'with a site result' do
-      let(:document) do
-        {
-          'title_short' => '0123456789abc',
-          'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
-          'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz'],
-          'dc_type_ssm' => ['Publish Target'],
-          'slug_ssim' => [slug]
+
+  describe '#total_dcv_asset_count' do
+    let (:solr_response) {
+      {
+        "response" => {
+          "numFound" => 12345,
+          "start" => 0,
+          "docs" => []
         }
-      end
-      it { is_expected.to eql('/sites/' + slug) }
+      }
+    }
+    it do
+      allow(Blacklight.solr).to receive(:get).and_return(solr_response)
+      expect(helper.total_dcv_asset_count).to eq(12345)
     end
-    context 'with a non-site result' do
-      let(:document) do
-        {
-          'title_short' => '0123456789abc',
-          'title_long' => '0123456789abcdefghijklmnopqrstuvwxyz',
-          'title_long_array' => ['0123456789abcdefghijklmnopqrstuvwxyz'],
-          'dc_type_ssm' => ['Unpublish Target'],
-          'slug_ssim' => [slug]
-        }
-      end
-      # until we configure routes in this helper config
-      it { is_expected.to be_a SolrDocument }
-    end
-    context 'with nil' do
-      subject { helper.url_for_document(nil) }
-      it { is_expected.to be_nil }
+  end
+
+  describe '#rounded_down_and_formatted_dcv_asset_count' do
+    it do
+      allow(helper).to receive(:total_dcv_asset_count).and_return(12345)
+      expect(helper.rounded_down_and_formatted_dcv_asset_count).to eq('10,000')
     end
   end
 end
