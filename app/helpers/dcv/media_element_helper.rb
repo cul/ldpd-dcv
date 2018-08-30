cfaba65bef8c6bfbc06e8270c2413fc382f0d9d7
Eric O <eric@ejdigitalmedia.com>
2018-08-29 20:09:09 -0400

    Version 1.2.5; Re-enable additional wowza video sources

--- app/helpers/dcv/media_element_helper.rb
+++ app/helpers/dcv/media_element_helper.rb
@@ -2,8 +2,8 @@ module Dcv::MediaElementHelper
   def render_media_element_streaming_video_player(wowza_project, video_path, poster_path, width=1024, height=576)
     return ('<div class="mejs-ted"><div class="mediaelement-player">
       <video width="' + width.to_s + '" height="' + height.to_s + '" style="width:100%;height:100%;" poster="' + poster_path + '" controls="controls" preload="none">
-        <!-- <source type="application/dash+xml" src="https://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '/manifest.mpd" /> -->
-        <!-- <source type="application/x-mpegURL" src="https://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '/playlist.m3u8" /> -->
+        <source type="application/dash+xml" src="https://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '/manifest.mpd" />
+        <source type="application/x-mpegURL" src="https://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '/playlist.m3u8" />
         <source type="video/rtmp" src="rtmps://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '" />
       </video>
     </div></div>').html_safe
