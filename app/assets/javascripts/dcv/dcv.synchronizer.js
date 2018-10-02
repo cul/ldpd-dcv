0ee820432295a07cf8a9c4b326bc64811d52f6aa
Benjamin Armintor <armintor@gmail.com>
2018-10-02 18:04:01 -0400

    pivot synchronizer modal on a mode param; use datastream urls for source

--- app/assets/javascripts/dcv/dcv.synchronizer.js
+++ app/assets/javascripts/dcv/dcv.synchronizer.js
@@ -1,27 +1,31 @@
 $(document).ready(function(){
   if($('#synchronizer-widget').length > 0) {
+
     var widgetOptions = {
         player: {
           type: 'video',
       		url: $('#synchronizer-widget').attr('data-media-url')
         },
-        transcript: {
-          id: 'input-transcript',
-          url: $('#synchronizer-widget').attr('data-captions-url')
-        },
-        index: {
-          id: 'input-index',
-          url: $('#synchronizer-widget').attr('data-index-document-url')
-        },
         options: {
           previewOnly: true
         }
-      };
-
-      var synchronizerWidget = new OHSynchronizer(widgetOptions);
-      OHSynchronizer.playerControls.bindNavControls(); //bind modal forward/back/etc. nav controls. TODO: Move this to widget js instead of DLC js
-      OHSynchronizer.errorHandler = function(e) {
-        alert(e);
+    };
+    if ($('#synchronizer-widget').attr('data-chapters-url')) {
+      widgetOptions.index = {
+        id: 'input-index',
+        url: $('#synchronizer-widget').attr('data-chapters-url')
       }
+    } else {
+      widgetOptions.transcript = {
+        id: 'input-transcript',
+        url: $('#synchronizer-widget').attr('data-captions-url')
+      }
+    }
+
+    var synchronizerWidget = new OHSynchronizer(widgetOptions);
+    OHSynchronizer.playerControls.bindNavControls(); //bind modal forward/back/etc. nav controls. TODO: Move this to widget js instead of DLC js
+    OHSynchronizer.errorHandler = function(e) {
+      alert(e);
+    }
   };
 });
