96384cfd4f2591d13bb709ec566b8e1987776b8b
Benjamin Armintor <armintor@gmail.com>
2019-03-01 10:34:32 -0500

    DLC-570/CARNEGIE-103 defer to content/original bytestream if filename is pdf

--- app/helpers/dcv/dcv_url_helper.rb
+++ app/helpers/dcv/dcv_url_helper.rb
@@ -31,9 +31,18 @@ module Dcv::DcvUrlHelper
     document['ezid_doi_ssim'].present?
   end
 
-  def preferred_content_bytestream(doc)
+  # Return the preferred bytestream name:
+  # 1. content if the original name matches a pattern in keep_originals
+  # 2. access if available
+  # 3. content by default
+  # @param doc [Hash] SolrDocument
+  # @param keep_originals [Array<Regexp>] name patterns to defer to original for
+  # @return [String] preferred bytestream name
+  def preferred_content_bytestream(doc, *keep_originals)
     doc = SolrDocument.new(doc) unless doc.nil? or doc.is_a? SolrDocument
     if doc.is_a?(SolrDocument)
+      originals = doc['original_name_ssim'] || doc[:original_name_ssim] || []
+      return 'content' if originals.detect {|o| keep_originals.detect {|k| k.match(o) } }
       datastreams = doc['datastreams_ssim'] || doc[:datastreams_ssim] || []
       return (['access','content'] & datastreams).first
     end
