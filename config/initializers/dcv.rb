9533f006e94ededdb227ca8a675116694347da5f
Benjamin Armintor <armintor@gmail.com>
2019-03-21 17:20:47 -0400

    Move geo field configs out of partials and into BL config (fixes DLC-586)

--- config/initializers/dcv.rb
+++ config/initializers/dcv.rb
@@ -1,3 +1,6 @@
 DCV_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/dcv.yml")[Rails.env].with_indifferent_access
 
 Sprockets::Context.send :include, Rails.application.routes.url_helpers
+
+# define field access for the map and geodata panels
+Blacklight::Configuration.define_field_access :geo_field
