509f41458af81611e540416a7eb61bf193c729f5
Benjamin Armintor <armintor@gmail.com>
2019-07-04 19:32:58 -0400

    v1.6.7 correct filter handling in map search queries

--- app/controllers/concerns/dcv/map_data_controller.rb
+++ app/controllers/concerns/dcv/map_data_controller.rb
@@ -10,20 +10,20 @@ module Dcv::MapDataController
 
   def set_map_data_json
     if has_search_parameters?
-      map_cache_key = subsite_key + '_map_search_data_json?'
-      params.sort {|a,b| a[0] <=> b[0] }.each do |k, v|
-        if k.starts_with?('f[') || k.eql?('q')
-          map_cache_key << '&' unless map_cache_key[-1] == '?'
-          map_cache_key << k << '=' << v
-        end
+      map_cache_key = subsite_key + '_map_search_data_json?q=' + params[:q].to_s
+
+      params[:f]&.sort {|a,b| a[0] <=> b[0] }.each do |k, vals|
+          vals.sort.each { |val| map_cache_key << '&' << k << '=' << val }
       end
     else
       map_cache_key = subsite_key + '_map_search_data_json'
     end
     @map_data_json = Rails.cache.fetch(map_cache_key)
     if @map_data_json.nil?
-      search_params = params.merge('f[coordinates][]' => '*')
-      (@response, @document_list) = get_search_results(search_params, map_response_params) # Calling get_search_results manually so that we always plot all points for the home page map
+      extra_controller_params = map_response_params
+      extra_controller_params[:f] = (params[:f].present? ? params[:f].dup : {})
+      extra_controller_params[:f][:geo] ||= ['*']
+      (@response, @document_list) = get_search_results(params, extra_controller_params) # Calling get_search_results manually so that we always plot all points for the home page map
       cache_expiration_time = Rails.env.development? ? 5.minutes : 1.day
       map_data = extract_map_data_from_document_list(@document_list)
       @map_data_json = map_data.to_json
