d89c9778a892294945e66bbfb023b5639fce69bc
Benjamin Armintor <armintor@gmail.com>
2019-09-26 10:04:55 -0400

    modify can_download? to use current ability semantics

--- app/helpers/dcv/ability_helper_behavior.rb
+++ app/helpers/dcv/ability_helper_behavior.rb
@@ -1,8 +1,7 @@
 module Dcv::AbilityHelperBehavior
   def can_download?(document=@document)
     if controller.subsite_config['show_original_file_download']
-      proxy = Cul::Omniauth::AbilityProxy.new(document_id: document[:id],remote_ip: request.remote_ip,publisher:document[:publisher_ssim])
-      can? :download, proxy
+      can? Ability::ACCESS_ASSET, document
     else
       false
     end
