f1f089d93c6ac7260a13ef2f348cc18c5ca30a0f
Benjamin Armintor <armintor@gmail.com>
2020-03-23 10:48:40 -0400

    fall back to document when evaluating download permissions, not NilClass

--- app/helpers/dcv/ability_helper_behavior.rb
+++ app/helpers/dcv/ability_helper_behavior.rb
@@ -1,5 +1,5 @@
 module Dcv::AbilityHelperBehavior
-  def can_download?(document=@document)
+  def can_download?(document)
     document = SolrDocument.new(document) unless document.is_a? SolrDocument
     if controller.subsite_config['show_original_file_download']
       can? Ability::ACCESS_ASSET, document
