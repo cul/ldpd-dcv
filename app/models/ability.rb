f1fce0c61f36371b326a16e439c7fe4c6f61547d
Benjamin Armintor <armintor@gmail.com>
2020-04-27 10:03:22 -0400

    disambiguate affiliation policy and remote onsite by affiliation in asset access

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,9 +28,12 @@ class Ability
         UNSPECIFIED_ACCESS_DECISION
       else
         result = false
-        if doc.fetch('access_control_levels_ssim',[]).include?(ACCESS_LEVEL_ONSITE)
+        if doc.fetch('access_control_levels_ssim',[]).include?(ACCESS_LEVEL_AFFILIATION)
           result ||= true if (doc.fetch('access_control_affiliations_ssim',[]) & affils).first
+        end
+        if doc.fetch('access_control_levels_ssim',[]).include?(ACCESS_LEVEL_ONSITE)
           result ||= true if (doc.fetch('access_control_locations_ssim',[]) & location_uris).first
+          result ||= remote_onsite_access_to_user?(doc, user, affils)
         end
         if doc.fetch('access_control_levels_ssim',[]).include?(ACCESS_LEVEL_EMBARGO)
           result ||= begin
@@ -38,19 +41,22 @@ class Ability
             DateTime.parse(release_date).httpdate <= Time.now.httpdate  if release_date
           end
         end
-        # if it is published to a site where the current user has explicit remote permissions
-        if !result && doc['publisher_ssim'].present?
-          doc['publisher_ssim'].each do |fedora_uri|
-            subsite_config = SubsiteConfig.for_fedora_uri(fedora_uri)
-            result ||= (subsite_config.fetch(:remote_ids, []).flatten.include?(user.uid)) if user
-            result ||= true if (subsite_config.fetch(:remote_roles,[]).flatten & affils).first
-            result
-            break if result
-          end
-        end
         result
       end
     end
+  end
+
+  # was this document published to a site where the current user has remote "onsite" permissions
+  def remote_onsite_access_to_user?(doc, user = nil, affils = [])
+    return false unless doc['publisher_ssim'].present?
+    remote_onsite_access = false
+    doc['publisher_ssim'].each do |fedora_uri|
+      subsite_config = SubsiteConfig.for_fedora_uri(fedora_uri)
+      remote_onsite_access ||= (subsite_config.fetch(:remote_ids, []).flatten.include?(user.uid)) if user
+      remote_onsite_access ||= true if (subsite_config.fetch(:remote_roles,[]).flatten & affils).first
+      break if remote_onsite_access
+    end
+    remote_onsite_access
   end
 
   def ip_to_location_uris(remote_ip)
