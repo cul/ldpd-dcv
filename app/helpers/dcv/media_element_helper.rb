c905dd1e69b5d3672c10b10bef7472b86ff0785b
Benjamin Armintor <armintor@gmail.com>
2020-05-16 09:08:17 -0400

    update comments and class names to make clear mejs not used

--- app/helpers/dcv/media_element_helper.rb
+++ app/helpers/dcv/media_element_helper.rb
@@ -1,10 +1,10 @@
 module Dcv::MediaElementHelper
   def render_media_element_streaming_video_player(wowza_project, video_path, poster_path, captions_path = nil, width=1024, height=576)
     if captions_path
-      # important not to mark up as default, or browser and mediaelement will both display
+      # important not to mark up as default, or browser and player will both display
       track_element = '<track label="English" kind="subtitles" srclang="en" src="' + captions_path + '" />'
     end
-    return ('<div class="mejs-ted"><div class="able-player">
+    return ('<div class="media-container"><div class="able-player">
       <video width="' + width.to_s + '" height="' + height.to_s + '" style="width:100%;height:100%;" poster="' + poster_path + '" data-able-player preload="auto">
         <source src="https://firehose.cul.columbia.edu:8443/' + wowza_project + '/_definst_/mp4:' + video_path + '/playlist.m3u8" />
         ' + track_element.to_s + '
@@ -14,10 +14,10 @@ module Dcv::MediaElementHelper
 
   def render_media_element_streaming_audio_player(url, poster_path, captions_path = nil, width=1024, height=576)
     if captions_path
-      # important not to mark up as default, or browser and mediaelement will both display
+      # important not to mark up as default, or browser and player will both display
       track_element = '<track label="English" kind="subtitles" srclang="en" src="' + captions_path + '" />'
     end
-    return ('<div class="mejs-ted"><div class="able-player">
+    return ('<div class="media-container"><div class="able-player">
       <audio width="' + width.to_s + '" style="width:100%;" data-able-player preload="auto">
         <source src="' + url + '" />' + track_element.to_s + '
       </audio>
@@ -26,10 +26,10 @@ module Dcv::MediaElementHelper
 
   def render_media_element_streaming_player(url, poster_path, captions_path = nil, width=1024, height=576)
     if captions_path
-      # important not to mark up as default, or browser and mediaelement will both display
+      # important not to mark up as default, or browser and player will both display
       track_element = '<track label="English" kind="subtitles" srclang="en" src="' + captions_path + '" />'
     end
-    return ('<div class="mejs-ted"><div class="able-player">
+    return ('<div class="media-container"><div class="able-player">
       <video width="' + width.to_s + '" height="' + height.to_s + '" style="width:100%;height:100%;" poster="' + poster_path + '" data-able-player preload="auto">
         <source src="' + url + '" />' + track_element.to_s + '
       </video>
@@ -38,10 +38,10 @@ module Dcv::MediaElementHelper
 
   def render_media_element_progressive_download_video_player(video_url, poster_path, captions_path = nil, width=1024, height=576)
     if captions_path
-      # important not to mark up as default, or browser and mediaelement will both display
+      # important not to mark up as default, or browser and player will both display
       track_element = '<track label="English" kind="subtitles" srclang="en" src="' + captions_path + '" />'
     end
-    return ('<div class="mejs-ted"><div class="able-player">
+    return ('<div class="media-container"><div class="able-player">
       <video width="' + width.to_s + '" height="' + height.to_s + '" style="width:100%;height:100%;" poster="' + poster_path + '" data-able-player preload="auto">
           <source type="video/mp4" src="' + video_url + '" />' + track_element.to_s + '
       </video>
@@ -50,7 +50,7 @@ module Dcv::MediaElementHelper
 
   def render_media_element_progressive_download_audio_player(audio_url, captions_path = nil)
     if captions_path
-      # important not to mark up as default, or browser and mediaelement will both display
+      # important not to mark up as default, or browser and player will both display
       track_element = '<track label="English" kind="subtitles" srclang="en" src="' + captions_path + '" />'
     end
     return (
