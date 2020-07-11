1b4da03b9e6af2ca8bd3097ee65a626951b62e03
Benjamin Armintor <armintor@gmail.com>
2020-07-11 00:02:58 -0400

    DRY highlight snippet logic

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -10,6 +10,7 @@ class SolrDocument
   ]
 
   include Blacklight::Solr::Document
+  include SolrDocument::Snippets
 
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension( Blacklight::Document::Email )
