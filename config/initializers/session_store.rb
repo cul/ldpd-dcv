c33326c176fddb0a101f52598505692cd2e784f0
Benjamin Armintor <armintor@gmail.com>
2020-10-23 18:11:54 -0400

    use cookie session store to avoid CSRF errors on form tests

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.development?
-  # In development, use cache_store so that we can push stacktraces into the session for debugging limits.  Cookies impose a data limit that is too low.
-  Dcv::Application.config.session_store :cache_store, key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml")['session_store_key']
-else
-  Dcv::Application.config.session_store :cookie_store, key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml")['session_store_key']
-end
+Dcv::Application.config.session_store :cookie_store, key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml")['session_store_key']
