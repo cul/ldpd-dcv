50b0b640c9e68474fd846010e8a71dad9f1b0572
Benjamin Armintor <armintor@gmail.com>
2020-10-30 15:41:55 -0400

    prevent custom site DOI routes from falling through to Sites::Search (DLC-762)

--- lib/dcv/routes/site_showable.rb
+++ lib/dcv/routes/site_showable.rb
@@ -1,6 +1,5 @@
 module Dcv
   class Routes::SiteShowable
-    DOI_ID_CONSTRAINT = { id: /10\.[A-Za-z0-9\-]+\/[^\/]+/ }
     def initialize(defaults = {})
       @defaults = defaults
     end
@@ -8,11 +7,11 @@ module Dcv
     def call(mapper, options = {})
       options = @defaults.merge(options)
 
-      mapper.get "/*id", controller: 'search', action: 'show', constraints: DOI_ID_CONSTRAINT
+      mapper.get "/*id", controller: 'search', action: 'show', constraints: Dcv::Routes::DOI_ID_CONSTRAINT
       # track is implemented in Blacklight and can use whatever the internal id is
       mapper.post "/:id/track", controller: 'search', action: 'track', as: 'track'
-      mapper.get "/*id/synchronizer", controller: 'search', action: 'synchronizer', as: "synchronizer", constraints: DOI_ID_CONSTRAINT
-      mapper.get "/previews/*id", controller: 'search', action: 'preview', as: "preview", constraints: DOI_ID_CONSTRAINT
+      mapper.get "/*id/synchronizer", controller: 'search', action: 'synchronizer', as: "synchronizer", constraints: Dcv::Routes::DOI_ID_CONSTRAINT
+      mapper.get "/previews/*id", controller: 'search', action: 'preview', as: "preview", constraints: Dcv::Routes::DOI_ID_CONSTRAINT
     end
   end
 end
--- spec/routing/catalog_controller_spec.rb
+++ spec/routing/catalog_controller_spec.rb
@@ -5,5 +5,11 @@ describe CatalogController, :type => :routing do
     it "routes to #index" do
       expect(:get => "/").to route_to(controller: "catalog", action:"home")
     end
+    it "routes pid ids to show action" do
+      expect(:get => "/catalog/cul:12345").to route_to(controller: "catalog", action:"show", id: "cul:12345")
+    end
+    it "routes doi ids to show action" do
+      expect(:get => "/catalog/10.123/1a2b-3c4d5e").to route_to(controller: "catalog", action:"show", id: "10.123/1a2b-3c4d5e")
+    end
   end
 end
