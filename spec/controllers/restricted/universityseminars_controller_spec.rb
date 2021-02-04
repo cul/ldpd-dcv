cde88aede3c8fe7b420a0f0fa15d81fae024656d
Benjamin Armintor <armintor@gmail.com>
2021-02-03 19:06:26 -0500

    refactor custom site subsite_key logic to allow restricted layout lookup (DLC-869)

---
+++ spec/controllers/restricted/universityseminars_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Restricted::UniversityseminarsController, :type => :controller do
+  describe '#subsite_styles' do
+    let(:api_key) { nil }
+    before do
+      FactoryBot.create(:site, slug: 'restricted/universityseminars', layout: 'custom', palette: 'custom')
+    end
+    it "includes both the common styles and custom styles" do
+      expect(controller.load_subsite.layout).to eql('custom')
+      expect(controller.subsite_layout).to eql('universityseminars')
+      expect(controller.subsite_styles).to eql(['universityseminars'])
+    end
+  end
+end
