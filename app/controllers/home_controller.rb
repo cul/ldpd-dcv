802f041f915dca722c2889b6e6b381d188cea685
Benjamin Armintor <armintor@gmail.com>
2021-02-06 10:28:54 -0500

    remove unnecessary blacklight code from HomeController, which only routes /restricted HTML

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,62 +1,35 @@
 # -*- encoding : utf-8 -*-
-require 'blacklight/catalog'
 
+# This controller currently only answers requests for the /restricted "home" page
 class HomeController < ApplicationController
 
-  include Blacklight::Catalog
-  include Dcv::Catalog::BrowseListBehavior
-  include Dcv::Catalog::DateRangeSelectorBehavior
-  include Dcv::Catalog::RandomItemBehavior
-  include Dcv::Catalog::ModsDisplayBehavior
   include Cul::Omniauth::AuthorizingController
   include Cul::Omniauth::RemoteIpAbility
+  include Dcv::Sites::ConfiguredLayouts
 
   before_filter :authorize_action, only:[:restricted]
   before_filter :store_unless_user
-  before_filter :set_browse_lists
 
-  layout 'dcv'
+  layout Proc.new { |controller|
+    self.subsite_layout
+  }
 
-  configure_blacklight do |config|
-    Dcv::Configurators::DcvBlacklightConfigurator.configure(config)
+  def subsite_config
+    {'layout' => 'gallery', 'slug' => 'restricted'}
   end
 
   def authorize_action
-    authorized = (SUBSITES['restricted'].keys - ['uri']).detect do |key|
-      c = "Restricted::#{(key + '_controller').camelcase}".constantize
-      can?(Ability::ACCESS_SUBSITE, c)
-    end
+    return true if current_ability.ip_to_location_uris(request.remote_ip).present?
 
-    if authorized
+    if current_user
       return true
     else
-      if current_user
-        access_denied(catalog_index_url)
-        return false
-      end
+      store_location
+      redirect_to_login
+      return false
     end
-    store_location
-    redirect_to_login
-    return false
-  end
-
-  # Overrides the Blacklight::Controller provided #search_action_url.
-  # By default, any search action from a Blacklight::Catalog controller
-  # should use the current controller when constructing the route.
-  def search_action_url options = {}
-    url_for(options.merge(:action => 'index', :controller=>'catalog'))
-  end
-
-  def index
   end
   
   def restricted
-  end
-  
-  private
-  
-  def set_browse_lists
-    @browse_lists = get_catalog_browse_lists
-  end
-  
+  end  
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe HomeController, type: :unit do
+	let(:controller) { described_class.new }
+	let(:params) {
+		ActionController::Parameters.new(
+		)
+	}
+	let(:request_double) { instance_double('ActionDispatch::Request') }
+	let(:remote_ip) { '255.255.255.255' }
+	before do
+		allow(request_double).to receive(:host).and_return('localhost')
+		allow(request_double).to receive(:optional_port)
+		allow(request_double).to receive(:protocol)
+		allow(request_double).to receive(:remote_ip).and_return(remote_ip)
+		allow(request_double).to receive(:path_parameters).and_return({})
+		allow(request_double).to receive(:flash).and_return({})
+		allow(controller).to receive(:params).and_return(params)
+		allow(controller).to receive(:request).and_return(request_double)
+	end
+
+	describe '#authorize_action' do
+		let(:location_uris) { [] }
+		let(:ability_double) { instance_double(Ability) }
+		before do
+			expect(controller).to receive(:current_ability).and_return(ability_double)
+			expect(ability_double).to receive(:ip_to_location_uris).with(remote_ip).and_return(location_uris)
+		end
+		context 'from any mapped location' do
+			let(:location_uris) { ['http://any.value'] }
+			it do
+				expect(controller).not_to receive(:current_user)
+				expect(controller.authorize_action).to be true
+			end
+		end
+		context 'with any current user' do
+			it do
+				expect(controller).to receive(:current_user).and_return(instance_double(User))
+				expect(controller.authorize_action).to be true
+			end
+		end
+		context 'with no location or user' do
+			it do
+				expect(controller).to receive(:current_user).and_return(nil)
+				expect(controller).to receive(:store_location)
+				expect(controller).to receive(:redirect_to_login)
+				expect(controller.authorize_action).to be false
+			end
+		end
+	end
+end
