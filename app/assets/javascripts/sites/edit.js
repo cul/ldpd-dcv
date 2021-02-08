2fde8ada03cfc27e6e4a32031dc3d5ed6e4a94c6
Benjamin Armintor <armintor@gmail.com>
2021-02-08 12:05:24 -0500

    delete unused function in sites/edit.js

--- app/assets/javascripts/sites/edit.js
+++ app/assets/javascripts/sites/edit.js
@@ -22,34 +22,6 @@ function addValueFieldsetFromTemplate(addButtonFieldset, templateName) {
 	});
 	newFieldset.insertBefore($(addButtonFieldset));
 }
-function addSiteImageUriFieldSet(addButtonFieldset) {
-	const newIndex = document.getElementsByName('site_image_uris').length;
-	// <fieldset name="site_image_uris" style="border-width: 0 0 2px 0;">
-	const newFieldset = document.createElement("fieldset");
-	newFieldset['name'] = 'site_image_uris';
-	newFieldset['className'] = 'paneled';
-	// <input value="" id="site_image_uris_1" type="text" name="site[image_uris][]">
-	const newInput = document.createElement("input");
-	newInput['type'] = 'text';
-	newInput['name'] = 'site[image_uris][]';
-	newInput['id'] = 'site_image_uris_' + newIndex;
-	newFieldset.appendChild(newInput);
-	// <input type="button" class="btn btn-danger" value="Remove" onclick="this.parentElement.remove();"/>
-	if (newIndex > 0) {
-		const newButton = document.createElement("button");
-		newButton['type'] = 'button';
-		newButton['className'] = 'btn btn-danger';
-		newButton['onclick'] = function() { this.parentElement.remove() };
-		const buttonSpan = document.createElement("span");
-		buttonSpan['className'] = 'glyphicon glyphicon-remove';
-		newButton.appendChild(buttonSpan);
-		newButton.appendChild(document.createTextNode(" Remove"));
-
-		newFieldset.appendChild(document.createTextNode(" "));
-		newFieldset.appendChild(newButton);
-	}
-	addButtonFieldset.parentNode.insertBefore(newFieldset, addButtonFieldset);
-}
 
 function addNavMenu(addButton) {
 	// build a template nav menu
