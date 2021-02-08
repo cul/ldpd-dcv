f76bf78229dd747ff85c5a5ef02d9a00841a583e
Benjamin Armintor <armintor@gmail.com>
2021-02-08 13:15:37 -0500

    spec Ability model for site access scenarios

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+describe Ability do
+	context 'restricted site' do
+		let(:remote_ip) { Rails.application.config_for(:location_uris).first[1]['remote_ip'].first }
+		let(:current_user) { FactoryBot.create(:user) }
+		let(:affils) { ['userAffil'] }
+		let(:locations) { Rails.application.config_for(:location_uris).keys }
+		let(:current_ability) { Ability.new(current_user, roles: affils, remote_ip: remote_ip) }
+		let(:site) { FactoryBot.create(:site, slug: "restricted/site_slug", restricted: true) }
+		it 'denies access' do
+			expect(current_ability.can?(Ability::ACCESS_SUBSITE, site)).to be false
+		end
+		context 'user is in remote IDs for site' do
+			before do
+				site.permissions.remote_ids = [current_user.uid]
+				site.save
+			end
+			it 'permits access' do
+				expect(current_ability.can?(Ability::ACCESS_SUBSITE, site)).to be true
+			end
+		end
+		context 'user affils in remote roles for site' do
+			before do
+				site.permissions.remote_roles = affils
+				site.save
+			end
+			it 'permits access' do
+				expect(current_ability.can?(Ability::ACCESS_SUBSITE, site)).to be true
+			end
+		end
+		context 'remote IP maps to location for site' do
+			before do
+				site.permissions.locations = locations
+				site.save
+			end
+			it 'permits access' do
+				expect(current_ability.can?(Ability::ACCESS_SUBSITE, site)).to be true
+			end
+		end
+	end
+
+end
