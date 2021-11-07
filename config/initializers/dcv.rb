13d27f919430b094c2090c7e7769101b7823beff
Benjamin Armintor <armintor@gmail.com>
2021-11-07 07:22:53 -0500

    configurable blacklight logger

--- config/initializers/dcv.rb
+++ config/initializers/dcv.rb
@@ -7,3 +7,7 @@ Blacklight::Configuration.define_field_access :geo_field
 
 # define field access for the citation panels
 Blacklight::Configuration.define_field_access :citation_field
+
+log_dev = Rails.root.join('log', "blacklight_#{Rails.env}.log")
+log_level = Blacklight.blacklight_yml.dig(Rails.env, 'logger', 'level') || 'warn'
+Blacklight.logger = ActiveSupport::Logger.new(log_dev, level: log_level.to_sym)
