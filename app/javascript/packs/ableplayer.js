18ac830944e8929e0df87b5884c85d07b694cb48
Benjamin Armintor <armintor@gmail.com>
2021-12-06 21:15:47 -0500

    elaborate error handling in Hls

--- app/javascript/packs/ableplayer.js
+++ app/javascript/packs/ableplayer.js
@@ -10,9 +10,11 @@ const loadHls = function () {
     var hls = new Hls({
       backBufferLength: Infinity,
       liveBackBufferLength: 90,
+//      debug: true,
     });
     hls.retried = false;
     hls.swapped = false;
+    const player = this;
     const media = this.media[0];
     hls.on(Hls.Events.MANIFEST_PARSED, function () {
       hls.attachMedia(media);
@@ -20,16 +22,28 @@ const loadHls = function () {
     hls.on(Hls.Events.ERROR, function (event, data) {
       if (!data.fatal) return;
       if (!data.err.type === Hls.ErrorTypes.MEDIA_ERROR) return;
-      if (!hls.retried) {
-        hls.retried = true;
-        hls.recoverMediaError();
-      } else if (!hls.swapped) {
-        hls.swapped = true;
-        hls.swapAudioCodec();
-        hls.recoverMediaError();
-      } else {
-        console.log({event, data, media: this.media.error});
-        hls.destroy();
+      console.log({event, data, media: this.media.error});
+      switch(data.details) {
+        case Hls.ErrorDetails.BUFFER_INCOMPATIBLE_CODECS_ERROR:
+          if (!hls.swapped) {
+            hls.swapped = true;
+            hls.swapAudioCodec();
+            hls.recoverMediaError();
+          }
+          return;
+        case Hls.ErrorDetails.BUFFER_APPEND_ERROR:
+        case Hls.ErrorDetails.BUFFER_APPENDING_ERROR:
+        case Hls.ErrorDetails.BUFFER_STALLED_ERROR:
+          if (!hls.retried) {
+            hls.retried = true;
+            hls.recoverMediaError();
+          } else {
+            console.log("already retried");
+          }
+          return;
+        default:
+          console.log(Hls.ErrorDetails);
+          hls.destroy();
       }
     });
     hls.loadSource(mediaUrl);
@@ -41,6 +55,8 @@ AblePlayer.prototype.setup = function() {
   loadHls.apply(this);
   toWrap.apply(this);
 }
+
+window.AblePlayer = AblePlayer;
 
 $(document).ready(function(){
   $('.able-player video, .able-player audio, video.able-player, audio.able-player').each(function(index, element){
