9cd643989d970218a968c530a026214235113605
Benjamin Armintor <armintor@gmail.com>
2021-12-16 15:34:33 -0500

    IIIF::PresentationsController uses BL7 fetch behaviors - DLC-956

--- app/controllers/concerns/dcv/catalog/cached_fetch_behavior.rb
+++ app/controllers/concerns/dcv/catalog/cached_fetch_behavior.rb
@@ -1,22 +1,17 @@
 module Dcv::Catalog::CachedFetchBehavior
   extend ActiveSupport::Concern
 
-  def fetch_from_cache(query_term, query_opts, expiry = 30.seconds)
+  def fetch_from_cache(query_term, query_opts, expiry = 30.seconds, force_refresh = false)
     cache_key = {term: query_term, opts: query_opts}
+    Rails.cache.delete(cache_key) if force_refresh
     doc_src = Rails.cache.fetch(cache_key, expires_in: expiry) do
-      r, doc = fetch query_term, query_opts
+      r, doc = fetch(query_term, query_opts)
       doc.to_h
     end
     SolrDocument.new(doc_src)
   end
 
   def fetch_and_refresh(query_term, query_opts, expiry = 30.seconds)
-    cache_key = {term: query_term, opts: query_opts}
-    Rails.cache.delete(cache_key)
-    doc_src = Rails.cache.fetch(cache_key, expires_in: expiry) do
-      r, doc = fetch query_term, query_opts
-      doc.to_h
-    end
-    SolrDocument.new(doc_src)
+    fetch_from_cache(query_term, query_opts, expiry, true)
   end
 end
