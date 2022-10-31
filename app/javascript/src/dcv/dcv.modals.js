e49ba6a756a4279e1106dfbfdeb954872e124a27
Benjamin Armintor <armintor@gmail.com>
2022-10-31 16:23:27 -0400

    replace colorbox with BS modals - project more information uses BS modal - item citations use BS modal - MODS xml display uses BS modal

---
+++ app/javascript/src/dcv/dcv.modals.js
@@ -0,0 +1,29 @@
+const DcvModals = {
+  titleFor: function(element) {
+    const titleFunc = $(element).data('modal-title-func');
+    if (titleFunc) {
+      return DcvModals[titleFunc].call(self, element);
+    }
+    const titleDelegate =  $(element).data('modal-title-delegate');
+    if (titleDelegate) return $(titleDelegate).html();
+    return $(element).data('modal-title'); 
+  },
+  downloadXmlTitle: function(element) {
+    const downloadUrl = $(element).data('download-url');
+    return '<a href="' + downloadUrl + '" data-no-turbolink="true"><span class="fa fa-download" aria-hidden="true"></span> Download XML</a>';
+  },
+  bodyFor: function(element) {
+    const displayUrl = $(element).data('display-url');
+    if (displayUrl) {
+      const fullscreen = ($(element).data('modal-fullscreen') == 'true') ? 'allowfullscreen' : '';
+      return '<div class="embed-responsive  embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + displayUrl + '" ' + fullscreen + '></iframe></div>';
+    }
+    const bodyDelegate = $(element).data('modal-body-delegate');
+    if (bodyDelegate) return $(bodyDelegate).html();
+    return $(element).data('modal-body');
+  },
+  needsEmbed: (element) => $(element).data('display-url'),
+  needsLarge: (element) => $(element).data('modal-large'),
+};
+
+export default DcvModals;
