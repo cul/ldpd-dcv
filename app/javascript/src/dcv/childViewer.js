dcf9bf27f957610f33365448062426c025ef1e51
Benjamin Armintor <armintor@gmail.com>
2022-11-09 18:05:10 -0500

    move all asset-specific show controls into child panel toolbar - only item-level controls in content title div - move MODS modal button generation into a helper

--- app/javascript/src/dcv/childViewer.js
+++ app/javascript/src/dcv/childViewer.js
@@ -24,34 +24,8 @@ export default function () {
     $('#child_gallery a[rel="item-link"].selected').removeClass('selected');
     $('#child_gallery a[rel="item-link"][data-child-number="' + childNumber + '"]').addClass('selected');
 
-    //Update title, zoom links
+    //Update title
     $('#child-viewer-subtitle').html($currentSlideElememt.attr('data-child-title'));
-    if ($currentSlideElememt.attr('data-has-details') == 'true') {
-      $('#child-zoom-modal-button').attr('data-display-url', $currentSlideElememt.attr('data-zoom-url')).show();
-      $('#child-zoom-new-window-button').attr('href', $currentSlideElememt.attr('data-zoom-url')).show();
-    } else {
-      $('#child-zoom-modal-button').hide();
-      $('#child-zoom-new-window-button').hide();
-    }
-
-    //Update download link
-    if ($currentSlideElememt.attr('data-has-iiif') == 'true') {
-      $('#download-button').attr('data-download-content-url', ''); //Clear out previous download content url value if present
-      $('#download-button').attr('data-iiif-info-url', $currentSlideElememt.attr('data-iiif-info-url'))
-      $('#download-button-group').show();
-    } else if ($currentSlideElememt.attr('data-download-content-url').length > 0) {
-      $('#download-button').attr('data-download-content-url', $currentSlideElememt.attr('data-download-content-url'));
-      $('#download-button-group').show();
-    } else {
-      $('#download-button-group').hide();
-    }
-
-    //Update item in context value, but don't show the link if it points to the current page
-    if ($currentSlideElememt.attr('data-object-in-context-url').length > 0 && window.location.href != $currentSlideElememt.attr('data-object-in-context-url')) {
-      $('#child-viewer-object-in-context').html('View Object in Context').attr('href', $currentSlideElememt.attr('data-object-in-context-url'));
-    } else {
-      $('#child-viewer-object-in-context').html('&nbsp;').attr('href', '#');
-    }
   }
   const carouselOpts = {
     interval: false // Do not automatically cycle
@@ -60,18 +34,12 @@ export default function () {
 
 
   $childCarouselElement.on('click', 'img.zoomable', function () {
-    $('#child-zoom-modal-button').click(); //Clicking on the image itself is the same as clicking on the modal zoom button
+    $childCarouselElement.find('.item-modal').click(); //Clicking on the image itself is the same as clicking on the modal zoom button
   });
 
   // Set up viewer gallert links
   $('#child_gallery a[rel="item-link"]').on('click', function () {
     $childCarouselElement.carousel(parseInt($(this).attr('data-child-number')));
-  });
-
-  //Set up new window zoomable image button
-  $('#child-zoom-new-window-button').on('click', function (e) {
-    e.preventDefault();
-    window.open($(this).attr('href'));
   });
 
   //Manually trigger slide load event for carousel so event function runs
