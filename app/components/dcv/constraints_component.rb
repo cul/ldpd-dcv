db2d5b8b639caabdff9ff76483e4512d9387ff98
Benjamin Armintor <armintor@gmail.com>
2023-02-16 15:44:32 -0500

    use small button group for constraint rendering in search results (DLC-1015)

--- app/components/dcv/constraints_component.rb
+++ app/components/dcv/constraints_component.rb
@@ -2,6 +2,13 @@
 
 module Dcv
   class ConstraintsComponent < Blacklight::ConstraintsComponent
+    def initialize(**args)
+      super
+      @start_over_component = nil
+      @query_constraint_component_options = { classes: "query btn-group-sm" }
+      @facet_constraint_component_options = { classes: "btn-group-sm" }
+    end
+
     def before_render
       with_additional_constraint do
         render date_range_constraint if render_date_range?
@@ -31,7 +38,7 @@ module Dcv
         value_ = end_year_label + ' or Earlier'
       end
       remove_path_ = url_for(helpers.search_state.params_for_search.except(:start_year, :end_year))
-      Blacklight::ConstraintLayoutComponent.new(label: 'Date Range', value: value_, remove_path: remove_path_)
+      Blacklight::ConstraintLayoutComponent.new(label: 'Date Range', value: value_, remove_path: remove_path_, classes: "btn-group-sm")
     end
 
     def render_coordinates?
@@ -41,7 +48,7 @@ module Dcv
     def coordinates_constraint
       value_ = params[:lat] + ',' + params[:long]
       remove_path_ = url_for(helpers.search_state.params_for_search.except(:lat, :long))
-      Blacklight::ConstraintLayoutComponent.new(label: 'Coordinates', value: value_, remove_path: remove_path_)
+      Blacklight::ConstraintLayoutComponent.new(label: 'Coordinates', value: value_, remove_path: remove_path_, classes: "btn-group-sm")
     end
 
     def render_durst?
@@ -50,7 +57,7 @@ module Dcv
 
     def durst_constraint
       remove_path_ = url_for(helpers.search_state.params_for_search.except(:durst_favorites))
-      Blacklight::ConstraintLayoutComponent.new(label: 'Show', value: "Seymour's Favorites", remove_path: remove_path_)
+      Blacklight::ConstraintLayoutComponent.new(label: 'Show', value: "Seymour's Favorites", remove_path: remove_path_, classes: "btn-group-sm")
     end
   end
 end
