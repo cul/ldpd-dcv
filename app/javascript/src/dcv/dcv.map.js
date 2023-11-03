29905249cbdca0fac25834ae893dff0cd6fb84c6
Benjamin Armintor <armintor@gmail.com>
2023-11-03 16:42:10 -0400

    patch to use free-tier stadia maps as advised by https://maps.stamen.com/stadia-partnership/

--- app/javascript/src/dcv/dcv.map.js
+++ app/javascript/src/dcv/dcv.map.js
@@ -37,7 +37,7 @@ export const initCulMapDisplayComponent = function($mapComponentDiv) {
 	$mapComponentDiv.height($(window).height()-300);
 	}
 
-	var tiles = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+	var tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png', {
 		maxZoom: maxZoomLevel,
 		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors.'
 	});
