edb9d430fc41cf584aeff7739c7e450cdb467fab
Benjamin Armintor <armintor@gmail.com>
2024-03-07 22:40:30 -0500

    use secure/samesite session cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml", aliases: true)['session_store_key']
+Rails.application.config.session_store :cookie_store,
+  key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml", aliases: true)['session_store_key'],
+  same_site: :none,
+  secure: true
