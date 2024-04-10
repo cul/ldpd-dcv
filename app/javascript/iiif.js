861ff26a39f876108ccd8590724e718143f7e93a
Benjamin Armintor <armintor@gmail.com>
2024-04-09 22:22:03 -0400

    do not display maximize button in single-manifest Mirador displays

--- app/javascript/iiif.js
+++ app/javascript/iiif.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
         window: {
           allowClose: false,
           allowFullscreen: true,
+          allowMaximize: false,
           panels: {
             info: true,
             canvas: true
