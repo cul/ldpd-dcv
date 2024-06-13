06ae82d6b3bd61d44df941ec093c110b653f4e6e
Benjamin Armintor <armintor@gmail.com>
2024-06-13 17:22:38 -0400

    use idiomatic secrets config names and add font-awesome pro scaffolding (DLC-1128)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,6 +2,6 @@
 
 cookie_opts = ["development", "test"].include?(Rails.env.to_s) ? {} : { same_site: :none, secure: true }
 Rails.application.config.session_store(:cookie_store,
-  key: YAML.load_file("#{Rails.root}/config/initializer_secrets.yml", aliases: true)['session_store_key'],
+  key: Rails.application.config.secret_key_base = Rails.application.config_for(:secrets)[:session_store_key],
   **cookie_opts
 )
