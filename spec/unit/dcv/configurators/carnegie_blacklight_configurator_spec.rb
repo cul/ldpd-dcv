912f53e6cc99a24ed666923bc32d1096e7df8aef
Benjamin Armintor <armintor@gmail.com>
2024-07-24 14:38:49 -0400

    fix spec descriptions for CarnegieBlacklightConfigurator (DLC-1140)

--- spec/unit/dcv/configurators/carnegie_blacklight_configurator_spec.rb
+++ spec/unit/dcv/configurators/carnegie_blacklight_configurator_spec.rb
@@ -13,7 +13,7 @@ describe Dcv::Configurators::CarnegieBlacklightConfigurator do
         described_class.configure(config)
       end
 
-      it 'only includes the field values in facet.field' do
+      it 'has a search field configuration for fulltext' do
         expect(config.search_fields['fulltext_tesim']).to be_a Blacklight::Configuration::SearchField
       end
     end
@@ -22,7 +22,7 @@ describe Dcv::Configurators::CarnegieBlacklightConfigurator do
         described_class.configure(config, fulltext: true)
       end
 
-      it 'only includes the field values in facet.field' do
+      it 'has a search field configuration for fulltext' do
         expect(config.search_fields['fulltext_tesim']).to be_a Blacklight::Configuration::SearchField
       end
     end
@@ -31,7 +31,7 @@ describe Dcv::Configurators::CarnegieBlacklightConfigurator do
         described_class.configure(config, fulltext: false)
       end
 
-      it 'only includes the field values in facet.field' do
+      it 'has no search field configuration for fulltext' do
         expect(config.search_fields['fulltext_tesim']).to be_nil
       end
     end
