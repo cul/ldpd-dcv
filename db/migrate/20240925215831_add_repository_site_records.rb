5736365c0c51b34d3b6c95dc5aa7154772a31ae9
Benjamin Armintor <armintor@gmail.com>
2024-09-25 19:14:40 -0400

    set up repository site records (DLC-1157)

---
+++ db/migrate/20240925215831_add_repository_site_records.rb
@@ -0,0 +1,17 @@
+class AddRepositorySiteRecords < ActiveRecord::Migration[6.1]
+  def change
+    reversible do |direction|
+      repository_ids = %w(NNC-A NNC-EA NNC-RB NyNyCAP NyNyCBL NyNyCMA)
+      direction.up do
+        repository_ids.each do |repository_id|
+          site = Site.create(slug: repository_id, layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
+          site.update(layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
+          site.save
+        end
+      end
+      direction.down do
+        repository_ids.each { |repository_id| Site.find_by(slug: repository_id)&.destroy }
+      end
+    end
+  end
+end
