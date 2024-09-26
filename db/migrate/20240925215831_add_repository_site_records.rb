8afb21fc3e9f0a7b9a8d40fe64c9be2db340b50b
Benjamin Armintor <armintor@gmail.com>
2024-09-26 01:01:16 -0400

    do not fail on existing site for repo id (DLC-1157)

--- db/migrate/20240925215831_add_repository_site_records.rb
+++ db/migrate/20240925215831_add_repository_site_records.rb
@@ -4,9 +4,13 @@ class AddRepositorySiteRecords < ActiveRecord::Migration[6.1]
       repository_ids = %w(NNC-A NNC-EA NNC-RB NyNyCAP NyNyCBL NyNyCMA)
       direction.up do
         repository_ids.each do |repository_id|
-          site = Site.create(slug: repository_id, layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
-          site.update(layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
-          site.save
+          site = Site.find_by(slug: repository_id)
+          if site
+            site.update(layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
+            site.save
+          else
+            site = Site.create(slug: repository_id, layout: Site::LAYOUT_REPOSITORIES, search_type: Site::SEARCH_REPOSITORIES)
+          end
         end
       end
       direction.down do
