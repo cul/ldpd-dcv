adc1642879244f9329cf42f0b3cfa11d181d7d3d
Benjamin Armintor <armintor@gmail.com>
2025-01-30 17:51:01 -0500

    Iiif::Authz::V2::Bytestreams#resource returns 401 status when auth is needed (DLC-1177)

--- app/controllers/concerns/iiif/authz/v2/bytestreams.rb
+++ app/controllers/concerns/iiif/authz/v2/bytestreams.rb
@@ -18,14 +18,22 @@ module Iiif::Authz::V2::Bytestreams
       return
     end
 
-   remote_ip = DCV_CONFIG.dig('media_streaming','wowza', 'client_ip_override') || request.remote_ip
-    probe_response = Iiif::Authz::V2::ProbeService::Response.new(document: @document, bytestream_id: params[:bytestream_id],ability_helper: self, route_helper: self, remote_ip: remote_ip).to_h
+    remote_ip = DCV_CONFIG.dig('media_streaming','wowza', 'client_ip_override') || request.remote_ip
+    probe_response = probe_service_response(
+      authorization: nil, bytestream_id: params[:bytestream_id], document: @document, remote_ip: remote_ip
+    ).to_h
     case(probe_response[:status])
     when 302
       redirect_to probe_response[:location]
     else
-      render json: probe_response
+      render nothing: true, status: probe_response[:status]
     end
+  end
+
+  def probe_service_response(bytestream_id:, document:, remote_ip:, authorization: nil)
+    Iiif::Authz::V2::ProbeService::Response.new(
+      document: document, bytestream_id: bytestream_id, ability_helper: self, route_helper: self,
+      remote_ip: remote_ip, authorization: authorization)
   end
 
   # IIIF Authorization 2.0 Probe Service
@@ -53,9 +61,10 @@ module Iiif::Authz::V2::Bytestreams
     end
 
     remote_ip = DCV_CONFIG.dig('media_streaming','wowza', 'client_ip_override') || request.remote_ip
-    probe_response = Iiif::Authz::V2::ProbeService::Response.new(
-      document: @document, bytestream_id: params[:bytestream_id], ability_helper: self, route_helper: self,
-      remote_ip: remote_ip, authorization: request.headers['Authorization']).to_h
+    authorization = request.headers['Authorization']
+    probe_response = probe_service_response(
+      authorization: authorization, bytestream_id: params[:bytestream_id], document: @document, remote_ip: remote_ip
+    ).to_h
     # IIIF Auth2 requires probe responses to have HTTP status 200, regardless of effective status in the response
     render json: probe_response, status: 200
   end
--- spec/controllers/concerns/iiif/authz/v2/bytestreams_spec.rb
+++ spec/controllers/concerns/iiif/authz/v2/bytestreams_spec.rb
@@ -6,9 +6,11 @@ describe Iiif::Authz::V2::Bytestreams, type: :unit do
       include Iiif::Authz::V2::Bytestreams
     end
   end
+
   subject(:controller) do
     test_class.new
   end
+
   describe '#has_probeable_resource?' do
     let(:bytestream_id) { 'content' }
     let(:params) { { bytestream_id: bytestream_id } }
@@ -56,5 +58,59 @@ describe Iiif::Authz::V2::Bytestreams, type: :unit do
         end
       end
     end
-  end  
+  end
+
+  describe '#resource' do
+    let(:authorization_header) { nil }
+    let(:bytestream_id) { 'content' }
+    let(:bytestream_content_url) { 'bytestream_content_url' }
+    let(:bytestream_probe_url) { 'bytestream_probe_url' }
+    let(:bytestream_token_url) { 'bytestream_token_url' }
+    let(:current_user) { nil }
+    let(:iiif_kiosk_url) { 'iiif_kiosk_url' }
+    let(:iiif_login_url) { 'iiif_login_url' }
+    let(:cache_control) { {} }
+    let(:solr_doc_dc_type) { 'Text' }
+    let(:request_headers) { instance_double(ActionDispatch::Http::Headers) }
+    let(:response_headers) { instance_double(ActionDispatch::Http::Headers) }
+    let(:origin_header) { 'localhost' }
+    let(:params) { { bytestream_id: bytestream_id } }
+    let(:remote_ip) { '0.0.0.0' }
+    let(:request) { instance_double(ActionDispatch::Request, headers: request_headers, remote_ip: remote_ip,
+      host: 'localhost', optional_port: nil, path_parameters: [], protocol: 'http') }
+    let(:response) { instance_double(ActionDispatch::Response, headers: response_headers, cache_control: cache_control) }
+    let(:solr_doc) { SolrDocument.new({
+      id: 'solr-1', dc_type_ssm: [solr_doc_dc_type], title_display_ssm: ['title_display_ssm'],
+      access_control_levels_ssim: [Dcv::AccessLevels::ACCESS_LEVEL_AFFILIATION]
+    }) }
+
+    before do
+      allow(controller).to receive_messages(
+        bytestream_content_url: bytestream_content_url, bytestream_probe_url: bytestream_probe_url, bytestream_token_url: bytestream_token_url,
+        current_user: current_user, fetch: [nil, solr_doc], iiif_kiosk_url: iiif_kiosk_url, iiif_login_url: iiif_login_url,
+        params: params, request: request, response: response, cors_headers: nil
+      )
+      allow(request_headers).to receive(:[]).with('Authorization').and_return(authorization_header)
+      allow(request_headers).to receive(:[]).with('Origin').and_return(origin_header)
+      allow(response_headers).to receive(:[]=).with('Cache-Control', String).and_return(true)
+      allow(response_headers).to receive(:[]=).with('Access-Control-Allow-Origin', origin_header).and_return(origin_header)
+      allow(response_headers).to receive(:[]=).with('Access-Control-Allow-Credentials', String).and_return(true)
+      allow(response_headers).to receive(:[]=).with('Content-Type', String).and_return(true)
+    end
+
+    context 'request is authorized' do
+      it 'redirects to probe response location with 302' do
+        allow(controller).to receive(:can?).and_return(true)
+        expect(controller).to receive(:redirect_to)
+        controller.resource
+      end
+    end
+    context 'request is denied' do
+      it 'responds with nothing and passes 401 through' do
+        allow(controller).to receive(:can?).and_return(false)
+        expect(controller).to receive(:render).with({nothing: true, status: 401})
+        controller.resource
+      end
+    end
+  end
 end
