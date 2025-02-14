e2a5b240d2678eff8a51e7639fe3bc7b47ad3e09
Eric O <eric@ejdigitalmedia.com>
2025-02-14 10:10:57 -0500

    Formatting update

--- spec/unit/dcv/utils/cdn_utils_spec.rb
+++ spec/unit/dcv/utils/cdn_utils_spec.rb
@@ -6,6 +6,7 @@ describe Dcv::Utils::CdnUtils, type: :unit do
     let(:url_encoded_file_path) { file_path.gsub(' ', '%20').gsub('&', '%26') }
     let(:single_slash_file_uri) { "file:#{url_encoded_file_path}" }
     let(:triple_slash_file_uri) { "file://#{url_encoded_file_path}" }
+
     it 'properly converts a ds location that uses a single slash file URI' do
       expect(described_class.file_uri_ds_location_to_file_path(single_slash_file_uri)).to eq(file_path)
     end
