11fcffefffa3625cab114c449c2c986219bfe561
goldsmithb <bg2918@columbia.edu>
2026-02-18 17:30:53 -0500

    better routing

--- app/frontend/src/components/layouts/sites-layout/sites-layout.tsx
+++ app/frontend/src/components/layouts/sites-layout/sites-layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import SitesNavbar from "./sites-navbar";
 
 const SitesLayout = () => {
   return (
     <div>
       <h2>Sites Layout</h2>
+      <SitesNavbar />
       <Outlet />
     </div>
   );
