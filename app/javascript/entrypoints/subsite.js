8e5d0d8f68e0da05110cc8cbf2ab10933b4da244
JackBlacklight <jd2148@columbia.edu>
2026-02-20 15:43:16 -0500

    restores modal js

--- app/javascript/entrypoints/subsite.js
+++ app/javascript/entrypoints/subsite.js
@@ -48,3 +48,39 @@ L.Icon.Default.mergeOptions({
 
 
 import 'leaflet.markercluster';
+
+import clipboardFromElement from "../src/features/dcv/clipboard";
+import DcvModals from "../src/features/dcv/dcv.modals";
+import { zoomingImageNewWindow } from "../src/features/dcv/modals/zoomingImage";
+import { initTiles } from '../src/features/dcv/dcv.show.zooming_viewer';
+
+window.clipboardFromElement = clipboardFromElement;
+window.initTiles = initTiles;
+window.zoomingImageNewWindow = zoomingImageNewWindow;
+
+$(document).ready(function(){
+  // make modals draggable
+  $("#dcvModal").draggable({
+    handle: "#dcvModalWrapper"
+  });
+  $('#dcvModal').on('show.bs.modal', function (event) {
+    const modalSizes = ['small', 'large', 'xl', 'max'];
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var modal = $(this);
+    modal.find('.modal-title').html(DcvModals.titleFor(button));
+    modal.find('.modal-body').html(DcvModals.bodyFor(button));
+    if (DcvModals.needsSize(button)) {
+      const modalSize = $(button).data('modal-size');
+      const modalDialog = modal.find('.modal-dialog');
+      modalSizes.forEach(size => (size == modalSize) ? modalDialog.addClass('modal-' + size) : modalDialog.removeClass('modal-' + size));
+    } else {
+      modalSizes.forEach(size => modal.find('.modal-dialog').removeClass('modal-' + size));
+    }
+    if (DcvModals.needsEmbed(button)) {
+      modal.find('.modal-content').addClass('mh-100');
+    } else {
+      modal.find('.modal-content').removeClass('mh-100');
+    }
+    modal.modal('handleUpdate');
+  });
+});
