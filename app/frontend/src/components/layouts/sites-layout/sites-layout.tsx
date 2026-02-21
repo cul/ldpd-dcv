4b07bf4814529e713c48f9132554ee00d2c3c212
goldsmithb <bg2918@columbia.edu>
2026-02-20 19:41:04 -0500

    wip: site properties route

--- app/frontend/src/components/layouts/sites-layout/sites-layout.tsx
+++ app/frontend/src/components/layouts/sites-layout/sites-layout.tsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
-import SitesNavbar from "./sites-navbar";
+import { Outlet, useLocation, useParams } from "react-router-dom";
+import SitesLayoutNavbar from "./sites-layout-navbar";
+import { Container } from "react-bootstrap";
+
 
 const SitesLayout = () => {
+  const { pathname } = useLocation();
+  const { slug } = useParams();
+
+  // Don't show navbar on subsite dashboard page
+  const showNav = pathname !== `/sites/${slug}`;
+  console.log(location.pathname)
+  console.log(showNav)
+
   return (
-    <div>
-      <h2>Sites Layout</h2>
-      <SitesNavbar />
+    <Container fluid className="border rounded border-info-subtle">
+      {showNav && <SitesLayoutNavbar />}
       <Outlet />
-    </div>
+    </Container>
   );
 };
 
