f40c119e6ec30e03eecbc57282865451883cb747
goldsmithb <bg2918@columbia.edu>
2026-02-26 11:59:58 -0500

    add prefetching on intent to sites index page

--- app/frontend/src/features/sites/components/sites-list.tsx
+++ app/frontend/src/features/sites/components/sites-list.tsx
@@ -3,10 +3,17 @@ import { ReactNode } from "react";
 import { useSitesSuspense } from "../api/get-sites";
 import { Link } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
+import { useQueryClient } from "@tanstack/react-query";
+import { getSiteQueryOptions } from "@/features/subsite/api/get-site";
 
 
 const SitesList = (): ReactNode => {
   const sites = useSitesSuspense();
+  const queryClient = useQueryClient();
+  const handleMouseEnter = (slug: string) => {
+    console.log(`            ON MOUSE ENTER! : PREFETCHING : /${slug}`);
+    queryClient.prefetchQuery(getSiteQueryOptions(slug));
+  }
 
   return (
     <Container fluid>
@@ -19,7 +26,7 @@ const SitesList = (): ReactNode => {
         <Row key={site.id}>
           <Col>{site.title}</Col>
           <Col>{site.slug}</Col>
-          <Col><Link to={site.slug}>Edit this site</Link></Col>
+          <Col><Link to={site.slug} onMouseEnter={() => handleMouseEnter(site.slug)}>Edit this site</Link></Col>
         </Row>
       ))}
     </Container>
