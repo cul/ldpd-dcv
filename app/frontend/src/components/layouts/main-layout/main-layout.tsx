7e6cc0f0e20a0a0e37dd2df14fb23cb54f23081c
goldsmithb <bg2918@columbia.edu>
2026-02-26 10:50:03 -0500

    add progress loading bar for fetching and nav

--- app/frontend/src/components/layouts/main-layout/main-layout.tsx
+++ app/frontend/src/components/layouts/main-layout/main-layout.tsx
@@ -1,25 +1,49 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useNavigation } from 'react-router-dom';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import AuthenticationBoundary from "@/components/auth/authentication-boundary";
 import MainNavBar from "./main-navbar";
 import Footer from "./footer";
 import FetchingSuspense from '@/components/ui/fetching-suspense';
+import { useIsFetching, useIsMutating } from '@tanstack/react-query';
 
+import TopBarProgress from "react-topbar-progress-indicator";
+
+TopBarProgress.config({
+  barColors: {
+    "0": "#ffffff",
+    "0.5": "#0DCAF0",
+    "1.0": "#ffffff",
+  },
+  shadowBlur: 0
+});
+
+
+const LoadingBar = () => {
+  const fetching = useIsFetching();``
+  const mutating = useIsMutating();
+  const navigation = useNavigation();
+
+  if (fetching > 0 || mutating > 0 || navigation.state !== 'idle') {
+    // return <div className="border border-primary">LOADING . . . </div>
+    return <TopBarProgress />
+  }
+}
 
 const MainLayout = () => {
   return (
     <AuthenticationBoundary>
       <div className="d-flex flex-column" style={{ minHeight: "100vh", }}>
+        <LoadingBar />
         {/* {import.meta.env.DEV && <ReactQueryDevtools />} */ /* This allows dev tools to be flush with footer (and not require scrolling) */}
         <header className="flex-grow-0 flex-shrink-0">
           <MainNavBar />
         </header>
         <div className="container-lg my-4 flex-grow-1">
 
-          <FetchingSuspense>
+          {/* <FetchingSuspense> */}
             <Outlet />
-          </FetchingSuspense>
+          {/* </FetchingSuspense> */}
 
         </div>
         <footer  className="flex-grow-0 flex-shrink-0 bg-dark-subtle">
