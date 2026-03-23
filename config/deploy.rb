af27fc000e09c26ed31a99049c42d2af85e6b714
goldsmithb <bg2918@columbia.edu>
2026-03-23 13:32:16 -0400

    upgrade to yarn v4

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,8 @@ append  :linked_files,
         'config/wind.yml',
         'config/reading_rooms.yml',
         'public/robots.txt',
-        '.npmrc'
+        '.npmrc',
+        '.yarnrc.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'public/images/sites', 'node_modules', 'public/packs', 'public/sitemaps'
@@ -66,7 +67,9 @@ set :rvm_ruby_version, fetch(:deploy_name) # This RVM alias must exist on the se
 end
 
 # Default value for default_env is {}
-set :default_env, NODE_ENV: 'production'
+set :default_env, {
+  NODE_ENV: 'production'
+}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
