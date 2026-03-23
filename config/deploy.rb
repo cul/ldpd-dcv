1c0d1c066d21fcdb771ba811da714733fec3d7be
goldsmithb <bg2918@columbia.edu>
2026-03-23 14:56:28 -0400

    enable corepack in capistrano deployment flow

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,6 +68,20 @@ set :rvm_ruby_version, fetch(:deploy_name) # This RVM alias must exist on the se
   )
 end
 
+# Add before the assets:precompile hook:
+namespace :deploy do
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute "nvm exec #{fetch(:nvm_node_version)} corepack enable"
+        execute "nvm exec #{fetch(:nvm_node_version)} yarn install --immutable"
+      end
+    end
+  end
+end
+
+before 'deploy:assets:precompile', 'deploy:yarn_install'
+
 # Default value for default_env is {}
 set :default_env, NODE_ENV: 'production'
 
