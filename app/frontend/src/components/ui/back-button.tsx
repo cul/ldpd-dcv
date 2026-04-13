adc858956c86cf4fa7d84f3a5ba9946531cc3aa2
goldsmithb <bg2918@columbia.edu>
2026-04-13 14:58:28 -0400

    adjust colors

--- app/frontend/src/components/ui/back-button.tsx
+++ app/frontend/src/components/ui/back-button.tsx
@@ -5,7 +5,7 @@ const BackButton = () => {
   const navigate = useNavigate();
 
   return (
-    <Button variant="secondary" className="fs-4 mt-4"
+    <Button variant="info text-white" className="fs-4 mt-4"
         onClick={() => navigate(-1)}
       >
         <i className="pe-3 fa-light fa-square-arrow-left"></i>
