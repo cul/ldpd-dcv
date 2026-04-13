d42abd95ca311c50e70268944551baf25f95f258
goldsmithb <bg2918@columbia.edu>
2026-04-13 09:07:41 -0400

    restyle back button for better contrast

--- app/frontend/src/components/ui/back-button.tsx
+++ app/frontend/src/components/ui/back-button.tsx
@@ -5,7 +5,7 @@ const BackButton = () => {
   const navigate = useNavigate();
 
   return (
-    <Button variant="outline-info" className="fs-4 mt-4"
+    <Button variant="secondary" className="fs-4 mt-4"
         onClick={() => navigate(-1)}
       >
         <i className="pe-3 fa-light fa-square-arrow-left"></i>
