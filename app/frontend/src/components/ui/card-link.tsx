122d4e7f59a474374ab8517c3240063aff6a3d3d
goldsmithb <bg2918@columbia.edu>
2026-04-14 09:16:05 -0400

    remove images, use fontawesome icons

--- app/frontend/src/components/ui/card-link.tsx
+++ app/frontend/src/components/ui/card-link.tsx
@@ -1,18 +1,17 @@
 import { ReactNode, useState } from "react";
 import { useNavigate } from "react-router"
-import { Col, Image, Row } from "react-bootstrap"
+import { Col, Row } from "react-bootstrap"
 
 
 type CardLinkProps = {
   to: string;
   label: string;
-  image?: string;
-  altTxt?: string;
+  faClass?: string;
 }
 
 
 
-const CardLink = ({ to, label, image, altTxt }: CardLinkProps): ReactNode => {
+const CardLink = ({ to, label, faClass }: CardLinkProps): ReactNode => {
   const navigate = useNavigate(); // Use use navigate to redirect user when they click on the card
   const [ hovering, setHovering ] = useState(false);
 
@@ -31,14 +30,12 @@ const CardLink = ({ to, label, image, altTxt }: CardLinkProps): ReactNode => {
       className={`m-2 m-lg-3 p-3 border border-3 border-info-subtle rounded ${hovering && 'bg-info-subtle'}`}
     >
       <Row className="text-secondary">
-        <Col xs={2} className="me-3">
-          <Image alt={altTxt} src={image} style={{ width: 80, }} />
+        <Col md={2} xs={12}>
+          <i className={faClass} style={{ fontSize: 'clamp(2em, 4vw, 5em)' }}></i>
         </Col>
-        <Col>
-        <div className="p-2">
+        <Col md={10} xs={12} className="p-2 ps-md-4 ps-5">
           {/* rightwards arrow: 8594 */}
           <h4>{String.fromCharCode(8680)} {label}</h4>
-        </div>
         </Col>
       </Row>
     </div>
