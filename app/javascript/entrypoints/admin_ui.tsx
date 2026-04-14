a3ced1c20e0aff130d35b38126b7f8b6317c1ef8
goldsmithb <bg2918@columbia.edu>
2026-04-14 08:50:45 -0400

    remove old entrypoint

--- app/javascript/entrypoints/admin_ui.tsx
+++ app/javascript/entrypoints/admin_ui.tsx
@@ -13,16 +13,13 @@
 // import '~/index.css'
 
 
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AdminApp from '../../frontend/src/app/AdminApp';
 import App from '../../frontend/src/app/App';
 
 const adminAppElement = document.getElementById('dlc-admin-app');
 if (!adminAppElement) throw new Error('Admin app root element not found');
 
 const adminRoot = createRoot(adminAppElement);
-// adminRoot.render(<AdminApp />);
 adminRoot.render(<App />)
 
 console.log('Admin React app load complete!');
