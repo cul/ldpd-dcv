4e6b4b9b10031a7e9b882eeb300f6a13d900314f
goldsmithb <bg2918@columbia.edu>
2026-04-28 15:43:14 -0400

    add auth to admin uploads controller

--- app/controllers/admin/uploads_controller.rb
+++ app/controllers/admin/uploads_controller.rb
@@ -1,24 +1,28 @@
+# frozen_string_literal: true
+
 module Admin
-  class UploadsController < ActionController::Base
+  # The admin uploads controller allows one to import a zipped subsite created by the export service
+  class UploadsController < ApplicationController
     layout 'admin'
     before_action :authenticate_user!
     before_action :authorize_import
 
     # GET /admin/import
-    def new
-    end
+    def new; end
 
     # POST /admin/upload
     def create
       begin
         uploaded_zip = params[:upload]
-        return if uploaded_zip.nil?
-        import = SubsiteImportService.new(uploaded_zip, current_user.admin?)
+        import = SubsiteImportService.new(uploaded_zip, current_user.is_admin?)
+
         import.import_subsite
-        flash[:success] = "#{import.finish_message} Your upload is complete and the site changes have been saved. If content is not available, please ensure the hyacinth publish target is correctly configured for your targeted DLC environment."
-      rescue Exception => err
-        flash[:error] = "An error occurred: #{err.message}"
+        flash[:success] =
+          "#{import.finish_message} Your upload is complete and the site changes have been saved. If content is not available, please ensure the hyacinth publish target is correctly configured for your targeted DLC environment."
+      rescue StandardError => e
+        flash[:error] = "An error occurred: #{e.message}"
       end
+
       redirect_to admin_import_path
     end
 
