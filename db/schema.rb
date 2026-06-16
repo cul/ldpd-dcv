57d5a3fde781a1263d2306a1eed1753ffde592b7
goldsmithb <bg2918@columbia.edu>
2026-06-16 11:08:10 -0400

    create new owner_uid column for sites and define association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2026_03_13_140719) do
+ActiveRecord::Schema.define(version: 2026_06_16_143646) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2026_03_13_140719) do
     t.boolean "show_facets", default: false
     t.text "editor_uids"
     t.text "search_configuration"
+    t.string "owner_uid", null: false
     t.index ["slug"], name: "index_sites_on_slug", unique: true
   end
 
