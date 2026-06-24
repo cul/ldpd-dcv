dfb45c1bc0aa665581d8e8c84cce6883899ab832
goldsmithb <bg2918@columbia.edu>
2026-06-24 15:36:09 -0400

    store default owner in config file

--- db/migrate/20260616143646_add_owner_to_sites.rb
+++ db/migrate/20260616143646_add_owner_to_sites.rb
@@ -1,6 +1,7 @@
 class AddOwnerToSites < ActiveRecord::Migration[6.1]
   def change
     add_column(:sites, :owner_uid, :string)
-    change_column_null(:sites, :owner_uid, false, 'bal35')
+    default_owner = Rails.application.config_for(:dcv).default_owner
+    change_column_null(:sites, :owner_uid, false, default_owner)
   end
 end
